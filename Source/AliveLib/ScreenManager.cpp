deac0917c81e947cb4503681e761c53e3b0f1d0c
Michael Grima <mlgthatsme@hotmail.com.au>
2018-07-07 02:32:51 +1000

    fixed func names

--- Source/AliveLib/ScreenManager.cpp
+++ Source/AliveLib/ScreenManager.cpp
@@ -45,14 +45,14 @@ void ScreenManager::dtor_40E460(signed int flags)
     NOT_IMPLEMENTED();
 }
 
-void ScreenManager::sub_40EC90(int x, int y, signed int width, signed int height, int a6)
+void ScreenManager::InvalidateRect_40EC90(int x, int y, signed int width, signed int height, int a6)
 {
     NOT_IMPLEMENTED();
 }
 
-void ScreenManager::InvalidateRect(int x, int y, signed int width, signed int height)
+void ScreenManager::InvalidateRect_40EC10(int x, int y, signed int width, signed int height)
 {
-    sub_40EC90(x, y, width, height, field_3A);
+    InvalidateRect_40EC90(x, y, width, height, field_3A);
 }
 
 ScreenManager::ScreenManager(int a2, int a3)
--- Source/AliveLib/ScreenManager.hpp
+++ Source/AliveLib/ScreenManager.hpp
@@ -38,8 +38,8 @@ public:
     EXPORT void sub_cam_vlc_40EF60(unsigned __int16** a2);
     EXPORT void MoveImage_40EB70();
 
-    EXPORT void sub_40EC90(int a2, int a3, signed int a4, signed int a5, int a6);
-    EXPORT void InvalidateRect(int a2, int a3, signed int a4, signed int a5);
+    EXPORT void InvalidateRect_40EC90(int x, int y, signed int width, signed int height, int a6);
+    EXPORT void InvalidateRect_40EC10(int x, int y, signed int width, signed int height);
 
     virtual void VDestructor(signed int flags) override;
     ScreenManager(int a2, int a3);
