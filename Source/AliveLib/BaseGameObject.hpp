d2d3d38573621d79900aa3462de86c6bf47486f7
Paul <Paul@paul.com>
2018-07-29 17:13:01 +0100

    delete commented out code

--- Source/AliveLib/BaseGameObject.hpp
+++ Source/AliveLib/BaseGameObject.hpp
@@ -88,37 +88,6 @@ ALIVE_ASSERT_SIZEOF(BaseGameObject, 0x20);
 
 ALIVE_VAR_EXTERN(DynamicArrayT<BaseGameObject>*, gBaseGameObject_list_BB47C4);
 
-//class BaseAnimatedWithPhysicsGameObject : public BaseGameObject
-//{
-//public:
-//    virtual void VDestructor(signed int flags);
-//    virtual void VUpdate() override;
-//
-//    EXPORT void dtor_424A40(char flags);
-//    EXPORT void dtor_424AD0();
-//    EXPORT void vnull_update_424AB0();
-//
-//public:
-//    AnimationEx field_20_animation;
-//    int field_B8_xpos;
-//    int field_BC_ypos;
-//    __int16 field_C0_path_number;
-//    __int16 field_C2_lvl_number;
-//    int field_C4_velx;
-//    int field_C8_vely;
-//    int field_CC_sprite_scale;
-//    __int16 field_D0_r;
-//    __int16 field_D2_g;
-//    __int16 field_D4_b;
-//    __int16 field_D6_scale;
-//    __int16 field_D8_yOffset;
-//    __int16 field_DA_xOffset;
-//    __int16 field_DC_bApplyShadows;
-//    __int16 field_DE_pad;
-//    int field_E0_176_ptr;
-//};
-//ALIVE_ASSERT_SIZEOF(BaseAnimatedWithPhysicsGameObject, 0xE4);
-
 class Class_5C1B70
 {
 public:
