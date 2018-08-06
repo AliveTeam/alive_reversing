f5746d1c3c8a37d3f05d213e7e2e1086007e5125
Michael Grima <mlgthatsme@hotmail.com.au>
2018-08-06 21:47:50 +1000

    impl SFX_Play_46FBA0 | impl SFX_Play_46FA90

--- Source/AliveLib/Sfx.cpp
+++ Source/AliveLib/Sfx.cpp
@@ -10,23 +10,28 @@ EXPORT int CC SFX_4CA420(SfxDefinition *a1, __int16 a2, __int16 min, __int16 max
     NOT_IMPLEMENTED();
 }
 
-int CC SFX_Play_46FBA0(unsigned __int8 /*a1*/, int /*a2*/, int /*a3*/, int /*a4*/)
+int CC SFX_Play_46FBA0(unsigned __int8 sfxIdx, __int16 volume, int a3, int scale)
 {
-    NOT_IMPLEMENTED();
+    if (!volume)
+    {
+        volume = (char)sSfxEntries_55C2A0[sfxIdx].field_3_default_volume;
+    }
+    if (scale == 0x8000)
+    {
+        volume /= 3;
+    }
+    return SFX_4CA420(&sSfxEntries_55C2A0[sfxIdx], volume, a3, a3);
 }
 
-int CC SFX_Play_46FA90(unsigned __int8 sfxIdx, __int16 a2, int a3)
+int CC SFX_Play_46FA90(unsigned __int8 sfxIdx, __int16 volume, int scale)
 {
-    __int16 v3; // ax
-
-    v3 = a2;
-    if (!a2)
+    if (!volume)
     {
-        v3 = (char)sSfxEntries_55C2A0[sfxIdx].field_3;
+        volume = sSfxEntries_55C2A0[sfxIdx].field_3_default_volume;
     }
-    if (a3 == 0x8000)
+    if (scale == 0x8000)
     {
-        v3 /= 3;
+        volume /= 3;
     }
-    return SFX_4CA420(&sSfxEntries_55C2A0[sfxIdx], v3, 0x7FFF, 0x7FFF);
+    return SFX_4CA420(&sSfxEntries_55C2A0[sfxIdx], volume, 0x7FFF, 0x7FFF);
 }
--- Source/AliveLib/Sfx.hpp
+++ Source/AliveLib/Sfx.hpp
@@ -7,12 +7,12 @@ struct SfxDefinition
     BYTE field_0_block_idx;
     BYTE field_1_program;
     BYTE field_2_note;
-    BYTE field_3;
+    BYTE field_3_default_volume;
     BYTE field_4;
     BYTE field_5;
     __int16 field_6_pitch_variation;
 };
 
 
-EXPORT int CC SFX_Play_46FBA0(unsigned __int8 a1, int a2, int a3, int a4);
-EXPORT int CC SFX_Play_46FA90(unsigned __int8 sfxIdx, __int16 a2, int a3);
+EXPORT int CC SFX_Play_46FBA0(unsigned __int8 sfxIdx, __int16 volume, int a3, int scale);
+EXPORT int CC SFX_Play_46FA90(unsigned __int8 sfxIdx, __int16 volume, int scale);
