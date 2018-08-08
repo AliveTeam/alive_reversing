72520715d258d6c19f3d8320d8640a07eaa477c7
Michael Grima <mlgthatsme@hotmail.com.au>
2018-08-08 16:50:50 +1000

    impl Vram_calc_width_4955A0

--- Source/AliveLib/VRam.cpp
+++ Source/AliveLib/VRam.cpp
@@ -2,6 +2,20 @@
 #include "VRam.hpp"
 #include "Function.hpp"
 
+EXPORT char CC Vram_calc_width_4955A0(int width, int depth)
+{
+    switch (depth)
+    {
+    case 0:
+        return ((width + 7) >> 2) & 0xFE;
+    case 1:
+        return ((width + 3) >> 1) & 0xFE;
+    case 2:
+        return (width + 1) & 0xFE;
+    }
+    return 0;
+}
+
 signed __int16 CC Vram_alloc_4956C0(unsigned __int16 width, __int16 height, unsigned __int16 colourDepth, PSX_RECT *pRect)
 {
     NOT_IMPLEMENTED();
