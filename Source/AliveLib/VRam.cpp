06d42f412c0c90fc9920d14eef5cfa57bc157335
Paul <Paul@paul.com>
2018-08-11 01:09:32 +0100

    impl Vram_rects_overlap_4959E0

--- Source/AliveLib/VRam.cpp
+++ Source/AliveLib/VRam.cpp
@@ -16,22 +16,46 @@ EXPORT char CC Vram_calc_width_4955A0(int width, int depth)
     return 0;
 }
 
-signed __int16 CC Vram_alloc_4956C0(unsigned __int16 width, __int16 height, unsigned __int16 colourDepth, PSX_RECT *pRect)
+signed __int16 CC Vram_alloc_4956C0(unsigned __int16 /*width*/, __int16 /*height*/, unsigned __int16 /*colourDepth*/, PSX_RECT* /*pRect*/)
 {
     NOT_IMPLEMENTED();
 }
 
-void CC Vram_free_495A60(int xy, int wh)
+void CC Vram_free_495A60(int /*xy*/, int /*wh*/)
 {
     NOT_IMPLEMENTED();
 }
 
-signed __int16 CC Pal_Allocate_483110(PSX_RECT *a1, unsigned int paletteColorCount)
+signed __int16 CC Pal_Allocate_483110(PSX_RECT* /*a1*/, unsigned int /*paletteColorCount*/)
 {
     NOT_IMPLEMENTED();
 }
 
-void CC Pal_free_483390(PSX_Point xy, __int16 palDepth)
+void CC Pal_free_483390(PSX_Point /*xy*/, __int16 /*palDepth*/)
 {
     NOT_IMPLEMENTED();
+}
+
+EXPORT BOOL CC Vram_rects_overlap_4959E0(const PSX_RECT* pRect1, const PSX_RECT* pRect2)
+{
+    const int x1 = pRect1->x;
+    const int x2 = pRect2->x;
+    if (x1 >= x2 + pRect2->w)
+    {
+        return 0;
+    }
+
+    const int y2 = pRect2->y;
+    const int y1 = pRect1->y;
+    if (y1 >= y2 + pRect2->h)
+    {
+        return 0;
+    }
+
+    if (x2 < x1 + pRect1->w)
+    {
+        return y2 < y1 + pRect1->h;
+    }
+
+    return 0;
 }
