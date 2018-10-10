cce05f61132bbea7ad5b1a988892bd1e19563753
Jay X Peet <jaytechmedia@googlemail.com>
2018-10-11 00:27:37 +0100

    Added EXPORT and SIZEOF Asserts.

--- Source/AliveLib/Slurg.cpp
+++ Source/AliveLib/Slurg.cpp
@@ -6,7 +6,7 @@ ALIVE_VAR(1, 0x5C1C08, WORD, sSlurg_Step_Watch_Points_Idx_5C1C08, 0);
 ALIVE_ARY(1, 0x5BD4DC, char, 2, sSlurg_Step_Watch_Points_Count_5BD4DC, {});
 ALIVE_ARY(1, 0x5C1B28, Slurg_Step_Watch_Points, 2, sSlurg_Step_Watch_Points_5C1B28, {});
 
-void CC Slurg::Clear_Slurg_Step_Watch_Points_449A90()
+EXPORT void CC Slurg::Clear_Slurg_Step_Watch_Points_449A90()
 {
     sSlurg_Step_Watch_Points_Idx_5C1C08 = sSlurg_Step_Watch_Points_Idx_5C1C08 == 0;
     sSlurg_Step_Watch_Points_Count_5BD4DC[sSlurg_Step_Watch_Points_Idx_5C1C08] = 0;
