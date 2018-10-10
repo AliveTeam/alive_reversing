88da41588554c54ec4920719e2c3235b32c6e354
Jay X Peet <jaytechmedia@googlemail.com>
2018-10-11 00:27:37 +0100

    Added EXPORT and SIZEOF Asserts.

--- Source/AliveLib/Slurg.hpp
+++ Source/AliveLib/Slurg.hpp
@@ -8,15 +8,17 @@ struct Slurg_Step_Watch_Point
     WORD field_0_xPos;
     WORD field_2_yPos;
 };
+ALIVE_ASSERT_SIZEOF(Slurg_Step_Watch_Point, 0x4);
 
 struct Slurg_Step_Watch_Points
 {
     Slurg_Step_Watch_Point field_0_points[5];
 };
+ALIVE_ASSERT_SIZEOF(Slurg_Step_Watch_Points, 0x14);
 
 
 class Slurg : public BaseAliveGameObject
 {
 public:
-    static void CC Clear_Slurg_Step_Watch_Points_449A90();
+    EXPORT static void CC Clear_Slurg_Step_Watch_Points_449A90();
 };
