59c35d5b3c9923ebd4e04c87658798ed115e43e0
Jay X Peet <jaytechmedia@googlemail.com>
2018-10-11 00:14:59 +0100

    Fixed tab/spaces. Used ALIVE_VAR/ARY. Other improvements.

--- Source/AliveLib/Slurg.cpp
+++ Source/AliveLib/Slurg.cpp
@@ -1,29 +1,19 @@
 #include "stdafx.h"
 #include "Slurg.hpp"
+#include "Function.hpp"
 
-WORD Slurg::sSlurg_Step_Watch_Points_Idx_5C1C08;
-char Slurg::sSlurg_Step_Watch_Points_Count_5BD4DC[2];
-Slurg_Step_Watch_Points Slurg::sSlurg_Step_Watch_Points_5C1B28[2];
+ALIVE_VAR(1, 0x5C1C08, WORD, sSlurg_Step_Watch_Points_Idx_5C1C08, 0);
+ALIVE_ARY(1, 0x5BD4DC, char, 2, sSlurg_Step_Watch_Points_Count_5BD4DC, {});
+ALIVE_ARY(1, 0x5C1B28, Slurg_Step_Watch_Points, 2, sSlurg_Step_Watch_Points_5C1B28, {});
 
 void CC Slurg::Clear_Slurg_Step_Watch_Points_449A90()
 {
-	sSlurg_Step_Watch_Points_Idx_5C1C08 = sSlurg_Step_Watch_Points_Idx_5C1C08 == 0;
-	sSlurg_Step_Watch_Points_Count_5BD4DC[sSlurg_Step_Watch_Points_Idx_5C1C08] = 0;
+    sSlurg_Step_Watch_Points_Idx_5C1C08 = sSlurg_Step_Watch_Points_Idx_5C1C08 == 0;
+    sSlurg_Step_Watch_Points_Count_5BD4DC[sSlurg_Step_Watch_Points_Idx_5C1C08] = 0;
 
-	Slurg_Step_Watch_Points* point = &sSlurg_Step_Watch_Points_5C1B28[sSlurg_Step_Watch_Points_Idx_5C1C08];
-	point->field_0_points[0].field_0_xPos = 0;
-	point->field_0_points[0].field_2_yPos = 0;
-	
-	point->field_0_points[1].field_0_xPos = 0;
-	point->field_0_points[1].field_2_yPos = 0;
 
-	point->field_0_points[2].field_0_xPos = 0;
-	point->field_0_points[2].field_2_yPos = 0;
-
-	point->field_0_points[3].field_0_xPos = 0;
-	point->field_0_points[3].field_2_yPos = 0;
-
-	point->field_0_points[4].field_0_xPos = 0;
-	point->field_0_points[4].field_2_yPos = 0;
+    for (auto& point : sSlurg_Step_Watch_Points_5C1B28[sSlurg_Step_Watch_Points_Idx_5C1C08].field_0_points)
+    {
+        point = {};
+    }
 }
-
--- Source/AliveLib/Slurg.hpp
+++ Source/AliveLib/Slurg.hpp
@@ -5,22 +5,18 @@
 
 struct Slurg_Step_Watch_Point
 {
-	WORD field_0_xPos;
-	WORD field_2_yPos;
+    WORD field_0_xPos;
+    WORD field_2_yPos;
 };
 
 struct Slurg_Step_Watch_Points
 {
-	Slurg_Step_Watch_Point field_0_points[5];
+    Slurg_Step_Watch_Point field_0_points[5];
 };
 
 
 class Slurg : public BaseAliveGameObject
 {
 public:
-	static WORD sSlurg_Step_Watch_Points_Idx_5C1C08;
-	static char sSlurg_Step_Watch_Points_Count_5BD4DC[2];
-	static Slurg_Step_Watch_Points sSlurg_Step_Watch_Points_5C1B28[2];
-
-	static void CC Clear_Slurg_Step_Watch_Points_449A90();
+    static void CC Clear_Slurg_Step_Watch_Points_449A90();
 };
