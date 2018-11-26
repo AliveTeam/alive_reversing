4715765aae251b68d19196ad2a3e38bcb4369350
Paul <Paul@paul.com>
2018-11-26 00:14:57 +0000

    add helper to script to allow raw int -> fp conversion

--- Scripts/Hex2Bits.py
+++ Scripts/Hex2Bits.py
@@ -1,9 +1,13 @@
+import math
+
 # LOBYTE = self
 # BYTE1 = + 00
 # BYTE2 = + 0000
 
 num = 6
 numAsBinaryString = bin(num)[2:]
+
+fp = 0x80000
 
 print "Converting " + hex(num) + " bits " + numAsBinaryString
 
@@ -13,4 +17,14 @@ for idx, zeroOrOne in enumerate(numAsBinaryString):
         print hex(int(splitBits, 2))
 print "Done"
 
+print ""
+
+print "Converting " + hex(fp) + " to fixed point calc"
+
+fpV = (fp / float(0x10000))
+if (math.floor(fpV) == fpV):
+    print "FP_FromInteger(" + str(int(fpV)) + ");"
+else:
+    print "FP_FromDouble(" + str(fpV) + ");"
+    
 #print hex(1<<21)
