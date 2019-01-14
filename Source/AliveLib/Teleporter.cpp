7189049e3ec234aad3a9226f4490a4608e796ab6
Paul <Paul@paul.com>
2019-01-14 20:46:53 +0000

    Correctly stub out ctor_41CF50 which should fix #26

--- Source/AliveLib/Teleporter.cpp
+++ Source/AliveLib/Teleporter.cpp
@@ -90,6 +90,7 @@ class ParticleBurst : public BaseAnimatedWithPhysicsGameObject
 public:
     EXPORT ParticleBurst* ctor_41CF50(FP /*xpos*/, FP /*ypos*/, unsigned int /*unknown_count1*/, FP /*scale*/, __int16 /*type*/, signed __int16 /*a7*/)
     {
+        NOT_IMPLEMENTED();
         return this;
     }
 private:
