9df9e8c30774d671a347df7690cbd396669a9f53
Michael Grima <mlgthatsme@hotmail.com.au>
2019-01-28 21:56:15 +1100

    wip SlamDoor::vUpdate_4AFD50

--- Source/AliveLib/SlamDoor.cpp
+++ Source/AliveLib/SlamDoor.cpp
@@ -4,6 +4,8 @@
 #include "stdlib.hpp"
 #include "Game.hpp"
 #include "SwitchStates.hpp"
+#include "Events.hpp"
+#include "Sfx.hpp"
 
 struct SlamDoor_Data
 {
@@ -61,6 +63,7 @@ TintEntry sSlamDoorTints_5603B0[18] =
 
 void SlamDoor::VUpdate()
 {
+    vUpdate_4AFD50();
 }
 
 SlamDoor * SlamDoor::ctor_4AF700(Path_SlamDoor * pTlv, TlvItemInfoUnion tlvInfo)
@@ -234,6 +237,235 @@ SlamDoor * SlamDoor::ctor_4AF700(Path_SlamDoor * pTlv, TlvItemInfoUnion tlvInfo)
     field_DC_bApplyShadows |= 2u;
 
     return this;
+}
+
+void SlamDoor::vUpdate_4AFD50()
+{
+    NOT_IMPLEMENTED();
+
+    bool stateUnchanged; // edi
+    PSX_RECT *v5; // edx
+    PSX_RECT *v6; // eax
+    PSX_Point v7; // ebp
+    FP v8; // edi
+    PSX_RECT *v11; // eax
+    int v14; // eax
+    PathLine *pPathLine; // eax
+    PSX_RECT *v27; // eax
+    PSX_RECT *v30; // eax
+    PSX_Point v31; // [esp+10h] [ebp-20h]
+    PSX_Point a4; // [esp+14h] [ebp-1Ch]
+    __int16 idx; // [esp+24h] [ebp-Ch]
+    PSX_RECT bRect; // [esp+28h] [ebp-8h]
+    idx = 0;
+    if (Event_Get_422C00(kEventDeathReset))
+    {
+        this->field_6_flags.Set(Options::eDead);
+    }
+    stateUnchanged = SwitchStates_Get_466020(this->field_128_switch_id) == static_cast<int>(field_118_flags.Get(SlamDoor_Flags_118::e118_Bit2_Open));
+    if (!(field_118_flags.Get(SlamDoor_Flags_118::e118_Bit1)))
+    {
+        if (this->field_20_animation.field_4_flags.Get(AnimFlags::eBit18_IsLastFrame))
+        {
+            if (this->field_20_animation.field_4_flags.Get(AnimFlags::eBit3_Render))
+            {
+                field_20_animation.field_4_flags.Clear(AnimFlags::eBit3_Render); // todo: check
+                if (field_118_flags.Get(SlamDoor_Flags_118::e118_Bit5_Delete))
+                {
+                    this->field_6_flags.Set(Options::eDead);
+                }
+                SFX_Play_46FBA0(0x39u, 100, 900, 0x10000);
+                SFX_Play_46FBA0(0x39u, 100, -100, 0x10000);
+                this->field_118_flags.Set(SlamDoor_Flags_118::e118_Bit3);
+            }
+        }
+    }
+    if (this->field_118_flags.Get(SlamDoor_Flags_118::e118_Bit1))
+    {
+        if (this->field_20_animation.field_4_flags.Get(AnimFlags::eBit18_IsLastFrame))
+        {
+            if (!field_118_flags.Get(SlamDoor_Flags_118::e118_Bit3))
+            {
+                field_118_flags.Set(SlamDoor_Flags_118::e118_Bit3);
+                SFX_Play_46FBA0(0x39u, 100, 900, 0x10000);
+                SFX_Play_46FBA0(0x39u, 100, -100, 0x10000);
+            }
+        }
+    }
+    if (stateUnchanged != field_118_flags.Get(SlamDoor_Flags_118::e118_Bit1))
+    {
+        // TODO: figure out and optimize this
+        this->field_118_flags.Raw().all = field_118_flags.Raw().all & ~4 ^ (LOBYTE(field_118_flags.Raw().all) & ~0xFF04u ^ (unsigned __int8)~(LOBYTE(field_118_flags.Raw().all) & ~4)) & 1;
+
+        if (stateUnchanged)
+        {
+            field_20_animation.field_4_flags.Set(AnimFlags::eBit3_Render); 
+
+            field_20_animation.Set_Animation_Data_409C80(
+                sSlamDoorData_547168[static_cast<int>(gMap_5C3030.sCurrentLevelId_5C3030)].field_8_frameTableOffset,
+                0);
+
+            if (field_CC_sprite_scale == FP_FromDouble(1.0))
+            {
+                const FP lineHeight = FP_FromDouble(80.0);
+
+                field_11C_pCollisionLine_6_2 = sCollisions_DArray_5C1128->Add_Dynamic_Collision_Line_417FA0(
+                    field_124_x1,
+                    FP_GetExponent(FP_FromInteger(field_126_y1) - lineHeight),
+                    field_124_x1,
+                    field_126_y1,
+                    2);
+                const FP x2 = FP_FromInteger(field_124_x1) + ScaleToGridSize_4498B0(field_CC_sprite_scale);
+                const FP y1 = FP_FromInteger(field_126_y1)
+                    - (field_CC_sprite_scale * FP_FromDouble(80.0));
+                const FP x1 = ScaleToGridSize_4498B0(field_CC_sprite_scale) + FP_FromInteger(field_124_x1);
+                pPathLine = sCollisions_DArray_5C1128->Add_Dynamic_Collision_Line_417FA0(
+                    FP_GetExponent(x1),
+                    FP_GetExponent(y1),
+                    FP_GetExponent(x2),
+                    field_126_y1,
+                    1);
+            }
+            else
+            {
+                const FP lineHeight = field_CC_sprite_scale * FP_FromDouble(80.0);
+
+                field_11C_pCollisionLine_6_2 = sCollisions_DArray_5C1128->Add_Dynamic_Collision_Line_417FA0(
+                    field_124_x1,
+                    FP_GetExponent(FP_FromInteger(field_126_y1) - lineHeight),
+                    field_124_x1,
+                    field_126_y1,
+                    6);
+                const FP x2 = FP_FromInteger(field_124_x1) + ScaleToGridSize_4498B0(field_CC_sprite_scale);
+                const FP y1 = FP_FromInteger(field_126_y1);
+                -(field_CC_sprite_scale * FP_FromDouble(80.0));
+                const FP x1 = ScaleToGridSize_4498B0(field_CC_sprite_scale) + FP_FromInteger(field_124_x1);
+                pPathLine = sCollisions_DArray_5C1128->Add_Dynamic_Collision_Line_417FA0(
+                    FP_GetExponent(x1),
+                    FP_GetExponent(y1),
+                    FP_GetExponent(x2),
+                    field_126_y1,
+                    5);
+            }
+
+            this->field_120_pCollisionLine_5_1 = pPathLine;
+            PSX_RECT v35;
+            v27 = GetBoundingRect_424FD0(
+                &v35,
+                1);
+            if (this->field_118_flags.Get(SlamDoor_Flags_118::e118_Bit4_Inverted))
+            {
+                v27->x += FP_GetExponent(FP_FromInteger(-110) * field_CC_sprite_scale);
+                //LOWORD(v28) = v32; // HMMM?
+                v27->w += FP_GetExponent(FP_FromInteger(-110) * field_CC_sprite_scale);
+            }
+            for (int i = 0; i < gBaseAliveGameObjects_5C1B7C->Size(); i++)
+            {
+                BaseAliveGameObject * pObj = gBaseAliveGameObjects_5C1B7C->ItemAt(i);
+
+                if (!pObj)
+                {
+                    break;
+                }
+
+                if (pObj->field_20_animation.field_4_flags.Get(AnimFlags::eBit3_Render))
+                {
+                    if (pObj->field_4_typeId != Types::eSlamDoor_122)
+                    {
+                        v30 = pObj->GetBoundingRect_424FD0(
+                            &bRect,
+                            1);
+
+                        // Todo: check this strange shit
+                        if (v27->y <= v30->w
+                            && v27->w >= (v30->x + 3)
+                            && v27->w >= v30->y
+                            && v27->x <= v30->h
+                            && pObj->field_CC_sprite_scale == this->field_CC_sprite_scale)
+                        {
+                            ClearInsideSlamDoor_4B0530(pObj, v27->y, v27->h);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            field_20_animation.Set_Animation_Data_409C80(
+                sSlamDoorData_547168[static_cast<int>(gMap_5C3030.sCurrentLevelId_5C3030)].field_0_frameTableOffset,
+                0);
+            Rect_Clear_418040((PSX_RECT *)this->field_11C_pCollisionLine_6_2);
+            v5 = (PSX_RECT *)this->field_120_pCollisionLine_5_1;
+            this->field_11C_pCollisionLine_6_2 = 0;
+            Rect_Clear_418040(v5);
+            this->field_120_pCollisionLine_5_1 = 0;
+        }
+    }
+    if (field_118_flags.Get(SlamDoor_Flags_118::e118_Bit1))
+    {
+        v6 = this->GetBoundingRect_424FD0(
+            &bRect,
+            1);
+        v7 = *(PSX_Point *)&v6->x;
+        v31 = *(PSX_Point *)&v6->x;
+        a4 = *(PSX_Point *)&v6->w;
+        if (this->field_118_flags.Get(SlamDoor_Flags_118::e118_Bit4_Inverted))
+        {
+            v31.field_2_y += FP_GetExponent(field_CC_sprite_scale * FP_FromInteger(-110));
+            v8 = field_CC_sprite_scale * FP_FromInteger(-110);
+            v7.field_0_x = v31.field_0_x;
+            a4.field_2_y += FP_GetExponent(v8) - FP_GetExponent(FP_FromInteger(20) * field_CC_sprite_scale);
+        }
+
+        for (int i = 0; i < gBaseAliveGameObjects_5C1B7C->Size(); i++)
+        {
+            BaseAliveGameObject * pObj = gBaseAliveGameObjects_5C1B7C->ItemAt(i);
+
+            if (!pObj)
+            {
+                break;
+            }
+
+            if (pObj->field_20_animation.field_4_flags.Get(AnimFlags::eBit3_Render))
+            {
+                if (pObj->field_4_typeId != Types::eSlamDoor_122 && pObj->field_4_typeId != Types::eGrenade_65)
+                {
+                    v11 = pObj->GetBoundingRect_424FD0(
+                        &bRect,
+                        1);
+
+                    v14 = FP_GetExponent(pObj->field_B8_xpos);
+
+                    if (v14 > v31.field_0_x
+                        && v14 < a4.field_0_x
+                        && v31.field_0_x <= v11->w
+                        && a4.field_0_x >= v11->x
+                        && a4.field_2_y >= v11->y
+                        && v31.field_2_y <= v11->h)
+                    {
+                        if (pObj->field_CC_sprite_scale == field_CC_sprite_scale
+                            || pObj->field_4_typeId == Types::eSlog_126 && field_CC_sprite_scale == FP_FromDouble(1.0))
+                        {
+                            ClearInsideSlamDoor_4B0530(pObj, v7.field_0_x, a4.field_0_x);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (this->field_20_animation.field_4_flags.Get(AnimFlags::eBit3_Render))
+    {
+        field_6_flags.Set(Options::eCanExplode);
+    }
+    else
+    {
+        field_6_flags.Clear(Options::eCanExplode);
+    }
+}
+
+void SlamDoor::ClearInsideSlamDoor_4B0530(BaseAnimatedWithPhysicsGameObject * /*pObj*/, __int16 /*a3*/, __int16 /*a4*/)
+{
+    NOT_IMPLEMENTED();
 }
 
 void SlamDoor_ForceLink()
--- Source/AliveLib/SlamDoor.hpp
+++ Source/AliveLib/SlamDoor.hpp
@@ -34,6 +34,9 @@ public:
     virtual void VUpdate() override;
 
     EXPORT SlamDoor* ctor_4AF700(Path_SlamDoor * tlv_params, TlvItemInfoUnion tlvInfo);
+    EXPORT void vUpdate_4AFD50();
+
+    EXPORT void ClearInsideSlamDoor_4B0530(BaseAnimatedWithPhysicsGameObject *pObj, __int16 a3, __int16 a4);
 
 public:
     __int16 field_116;
