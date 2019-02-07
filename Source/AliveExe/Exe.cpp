6f3f1625d979691c15ad46f893195ce71158c48c
paul <Paul@paul.com>
2019-02-07 20:23:50 +0000

    more gcc fixes/hacks

--- Source/AliveExe/Exe.cpp
+++ Source/AliveExe/Exe.cpp
@@ -1,14 +1,10 @@
-#include <windows.h>
 #include <iostream>
-#include "WinMain.hpp"
-#include "ExportHooker.hpp"
-
-bool IsAlive()
-{
-    return false;
-}
 
 #if _WIN32
+#include <windows.h>
+#include "ExportHooker.hpp"
+#include "WinMain.hpp"
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     AllocConsole();
@@ -25,9 +21,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return WinMain_4EE631(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 }
 #else
+
+using DWORD = unsigned int;
+using LPSTR = const char*;
+using HINSTANCE = void*;
+
+#define CALLBACK
+
+#include "WinMain.hpp"
+
 int main()
 {
     // TOOD: Convert command line arguments to what WinMain_4EE631 expects
     return WinMain_4EE631(0, 0, 0, 1);
 }
 #endif
+
+bool IsAlive()
+{
+    return false;
+}
