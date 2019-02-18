8585b80ad0b060675245ba9c6dd6a06e85aa3cf5
paul <Paul@paul.com>
2019-02-18 23:08:47 +0000

    impl PullRingRope::ctor_49B2D0

--- Source/AliveLib/PullRingRope.cpp
+++ Source/AliveLib/PullRingRope.cpp
@@ -1,6 +1,95 @@
 #include "stdafx.h"
 #include "PullRingRope.hpp"
+#include "BaseAliveGameObject.hpp"
+#include "Rope.hpp"
 #include "Function.hpp"
+#include "stdlib.hpp"
+
+const TintEntry sPullRingRopeTints_55FD1C[18] =
+{
+    { 1u, 127u, 127u, 127u },
+    { 2u, 127u, 127u, 127u },
+    { 3u, 127u, 127u, 127u },
+    { 4u, 127u, 127u, 127u },
+    { 5u, 127u, 127u, 127u },
+    { 6u, 127u, 127u, 127u },
+    { 7u, 127u, 127u, 127u },
+    { 8u, 127u, 127u, 127u },
+    { 9u, 127u, 127u, 127u },
+    { 10u, 127u, 127u, 127u },
+    { 11u, 127u, 127u, 127u },
+    { 12u, 127u, 127u, 127u },
+    { 13u, 127u, 127u, 127u },
+    { 14u, 127u, 127u, 127u },
+    { -1, 127u, 127u, 127u },
+    { 0u, 0u, 0u, 0u },
+    { 0u, 0u, 0u, 0u },
+    { 0u, 0u, 0u, 0u }
+};
+
+
+PullRingRope* PullRingRope::ctor_49B2D0(Path_PullRingRope* pTlv, int tlvInfo)
+{
+    BaseAnimatedWithPhysicsGameObject_ctor_424930(0);
+    SetVTable(this, 0x546A00);
+
+    field_4_typeId = Types::ePullRope_103;
+
+    BYTE** v4 = Add_Resource_4DC130(ResourceManager::Resource_Animation, ResourceID::kPullringResID);
+    Animation_Init_424E10(3020, 35, 0x16u, v4, 1, 1);
+
+    SetTint_425600(sPullRingRopeTints_55FD1C, gMap_5C3030.sCurrentLevelId_5C3030);
+
+    field_20_animation.field_4_flags.Set(AnimFlags::eBit15_bSemiTrans);
+    field_B8_xpos = FP_FromInteger((pTlv->field_8_top_left.field_0_x + pTlv->field_C_bottom_right.field_0_x) / 2);
+    field_BC_ypos = FP_FromInteger(pTlv->field_8_top_left.field_2_y + 24);
+
+    field_102_id = pTlv->field_10_id;
+    field_104_target_action = pTlv->field_12_target_action;
+    field_110_tlvInfo = tlvInfo;
+    field_100_state = 0;
+    field_F4_stay_in_state_ticks = 0;
+
+    const FP ropeLength = FP_FromInteger(pTlv->field_14_length_of_rope);
+    field_BC_ypos += ropeLength;
+
+    if (pTlv->field_16_scale == 1)
+    {
+        field_CC_sprite_scale = FP_FromDouble(0.5);
+        field_20_animation.field_C_render_layer = 8;
+        field_D6_scale = 0;
+    }
+    else
+    {
+        field_CC_sprite_scale = FP_FromInteger(1);
+        field_20_animation.field_C_render_layer = 27;
+        field_D6_scale = 1;
+    }
+
+    field_B8_xpos = FP_FromInteger(SnapToXGrid_449930(field_CC_sprite_scale, FP_GetExponent(field_B8_xpos)));
+
+    field_106_on_sound = pTlv->field_18_on_sound;
+    field_108_off_sound = pTlv->field_1A_off_sound;
+    field_10A_sound_direction = pTlv->field_1C_sound_direction;
+    field_FC_ring_puller_id = -1;
+
+    auto pRope = alive_new<Rope>();
+    if (pRope)
+    {
+        pRope->ctor_4A0A70(
+            FP_GetExponent(field_B8_xpos + FP_FromInteger(2)),
+            FP_GetExponent(field_BC_ypos - ropeLength),
+            FP_GetExponent(field_BC_ypos),
+            field_CC_sprite_scale);
+
+        field_F8_rope_id = pRope->field_8_object_id;
+    }
+
+    pRope->field_BC_ypos = FP_NoFractional(field_BC_ypos- (field_CC_sprite_scale * FP_FromInteger(16)));
+
+    field_DC_bApplyShadows |= 2;
+    return this;
+}
 
 BaseGameObject* PullRingRope::VDestructor(signed int flags)
 {
@@ -56,7 +145,7 @@ __int16 PullRingRope::vPull_49BBD0(BaseGameObject* /*a2*/)
 
 BOOL PullRingRope::vsub_49BC90()
 {
-    return field_100 != 1;
+    return field_100_state != 1;
 }
 
 void PullRingRope::vsub_49B610()
--- Source/AliveLib/PullRingRope.hpp
+++ Source/AliveLib/PullRingRope.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
 #include "BaseAnimatedWithPhysicsGameObject.hpp"
+#include "Path.hpp"
 #include "FunctionFwd.hpp"
+
+struct Path_PullRingRope : public Path_TLV
+{
+    __int16 field_10_id;
+    __int16 field_12_target_action;
+    __int16 field_14_length_of_rope;
+    __int16 field_16_scale;
+    __int16 field_18_on_sound;
+    __int16 field_1A_off_sound;
+    __int16 field_1C_sound_direction;
+    // pad
+};
+ALIVE_ASSERT_SIZEOF_ALWAYS(Path_PullRingRope, 0x20);
 
 class PullRingRope : public BaseAnimatedWithPhysicsGameObject
 {
 public:
+    EXPORT PullRingRope* ctor_49B2D0(Path_PullRingRope* pTlv, int tlvInfo);
+
     virtual BaseGameObject* VDestructor(signed int flags) override;
     virtual void VUpdate() override;
     virtual void VScreenChanged() override;
@@ -20,20 +36,17 @@ private:
     EXPORT BOOL vsub_49BC90();
     EXPORT void vsub_49B610();
 private:
-    int field_E4;
-    int field_E8;
-    int field_EC;
-    int field_F0;
-    int field_F4;
-    int field_F8;
-    int field_FC;
-    __int16 field_100;
-    __int16 field_102;
-    __int16 field_104;
-    __int16 field_106;
-    __int16 field_108;
-    __int16 field_10A;
+    int field_E4_not_used[4];
+    int field_F4_stay_in_state_ticks;
+    int field_F8_rope_id;
+    int field_FC_ring_puller_id;
+    __int16 field_100_state;
+    __int16 field_102_id;
+    __int16 field_104_target_action;
+    __int16 field_106_on_sound;
+    __int16 field_108_off_sound;
+    __int16 field_10A_sound_direction;
     int field_10C;
-    int field_110;
+    int field_110_tlvInfo;
 };
 ALIVE_ASSERT_SIZEOF(PullRingRope, 0x114);
