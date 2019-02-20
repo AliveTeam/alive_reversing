6bf28f80789191f3d60b5134e21e2d9ca0ae6492
paul <Paul@paul.com>
2019-02-20 23:21:56 +0000

    impl LiftMover::vUpdate_40CE20

--- Source/AliveLib/LiftMover.cpp
+++ Source/AliveLib/LiftMover.cpp
@@ -3,6 +3,9 @@
 #include "Function.hpp"
 #include "BaseAliveGameObject.hpp"
 #include "LiftPoint.hpp"
+#include "Events.hpp"
+#include "SwitchStates.hpp"
+#include "ObjectIds.hpp"
 #include "stdlib.hpp"
 
 EXPORT LiftMover* LiftMover::ctor_40CCD0(Path_LiftMover* pTlv, int tlvInfo)
@@ -69,7 +72,152 @@ int LiftMover::VGetSaveState(BYTE* pSaveBuffer)
 
 void LiftMover::vUpdate_40CE20()
 {
-    NOT_IMPLEMENTED();
+    LiftPoint* pLift = static_cast<LiftPoint*>(sObjectIds_5C1B70.Find_449CF0(field_28_lift_id));
+    if (field_32_bMoveInProgress)
+    {
+        pLift = GetLiftPoint_40D0F0();
+        if (!pLift)
+        {
+            return;
+        }
+        field_32_bMoveInProgress = 0;
+    }
+
+    if (pLift && pLift->field_6_flags.Get(BaseGameObject::eDead))
+    {
+        // Set extra dead for good measure
+        field_6_flags.Set(BaseGameObject::eDead);
+        field_28_lift_id = -1;
+    }
+    else
+    {
+        switch (field_30_state)
+        {
+        case 0:
+            if (SwitchStates_Get_466020(field_20_enabled_by_switch_id))
+            {
+                if (!pLift)
+                {
+                    pLift = GetLiftPoint_40D0F0();
+                }
+
+                if (pLift)
+                {
+                    field_28_lift_id = pLift->field_8_object_id;
+                    field_30_state = 1;
+                }
+            }
+            break;
+
+        case 1:
+            if (!pLift)
+            {
+                return;
+            }
+
+            if (!pLift->vOnAnyFloor_461920())
+            {
+                field_30_state = 2;
+                pLift->vKeepOnMiddleFloor_461870();
+            }
+            else
+            {
+                pLift->vMove_4626A0(FP_FromInteger(0), field_2C_speed, 0);
+                if (field_2C_speed < FP_FromInteger(0))
+                {
+                    if (pLift->vOnTopFloor_461890())
+                    {
+                        field_30_state = 2;
+                    }
+                }
+            }
+            break;
+
+        case 2:
+            if (!pLift)
+            {
+                return;
+            }
+
+            if (!pLift->vOnAFloorLiftMoverCanUse_461960())
+            {
+                pLift->vMove_4626A0(FP_FromInteger(0), field_2C_speed, 0);
+            }
+            else
+            {
+                pLift->vMove_4626A0(FP_FromInteger(0), FP_FromInteger(0), 0);
+                field_30_state = 5;
+            }
+            break;
+
+        case 3:
+            if (!pLift)
+            {
+                return;
+            }
+
+            if (pLift->vOnAFloorLiftMoverCanUse_461960())
+            {
+                pLift->vMove_4626A0(FP_FromInteger(0), field_2C_speed, 0);
+                if (field_2C_speed < FP_FromInteger(0))
+                {
+                    if (pLift->vOnTopFloor_461890())
+                    {
+                        field_30_state = 2;
+                    }
+                }
+
+                if (field_2C_speed > FP_FromInteger(0))
+                {
+                    if (pLift->vOnBottomFloor_4618F0())
+                    {
+                        field_30_state = 2;
+                    }
+                }
+            }
+            else
+            {
+                field_30_state = 4;
+                pLift->vKeepOnMiddleFloor_461870();
+            }
+            break;
+
+        case 4:
+            if (!pLift)
+            {
+                return;
+            }
+
+            if (pLift->vOnAFloorLiftMoverCanUse_461960())
+            {
+                pLift->vMove_4626A0(FP_FromInteger(0), FP_FromInteger(0), 0);
+
+                field_30_state = 0;
+                field_2C_speed = -field_2C_speed;
+            }
+            else
+            {
+                pLift->vMove_4626A0(FP_FromInteger(0), field_2C_speed, 0);
+            }
+            break;
+
+        case 5:
+            if (!SwitchStates_Get_466020(field_20_enabled_by_switch_id))
+            {
+                field_30_state = 3;
+                field_2C_speed = -field_2C_speed;
+            }
+            break;
+
+        default:
+            break;
+        }
+
+        if (Event_Get_422C00(kEventDeathReset))
+        {
+            field_6_flags.Set(BaseGameObject::eDead);
+        }
+    }
 }
 
 void LiftMover::dtor_40CDA0()
