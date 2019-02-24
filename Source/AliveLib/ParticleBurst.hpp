996120be52e8d3226c753100a12bcde67e76f544
paul <Paul@paul.com>
2019-02-24 12:51:09 +0000

    impl ParticleBurst::ctor_41CF50 impl ParticleBurst::Random_Speed_41CEE0 impl ParticleBurst::vdtor_41D4E0 impl ParticleBurst::dtor_41D510

--- Source/AliveLib/ParticleBurst.hpp
+++ Source/AliveLib/ParticleBurst.hpp
@@ -3,18 +3,31 @@
 #include "BaseAnimatedWithPhysicsGameObject.hpp"
 #include "FunctionFwd.hpp"
 
+struct ParticleBurst_Item;
+
 class ParticleBurst : public BaseAnimatedWithPhysicsGameObject
 {
 public:
     EXPORT ParticleBurst* ctor_41CF50(FP xpos, FP ypos, unsigned int unknown_count1, FP scale, __int16 type, signed __int16 a7);
+    virtual BaseGameObject* VDestructor(signed int flags) override;
+    virtual void VUpdate() override;
+    virtual void VRender(int** pOrderingTable) override;
+
+private:
+    EXPORT FP* Random_Speed_41CEE0(FP* random);
+    EXPORT ParticleBurst* vdtor_41D4E0(signed int flags);
+    EXPORT void dtor_41D510();
+    EXPORT void vRender_41D7B0(int** pOt);
+    EXPORT void vUpdate_41D590();
+
 private:
     int field_E4_not_used[4];
-    BYTE** field_F4_ppResBuffer;
-    void* field_F8_ptr_unknown;
-    __int16 field_FC_count1;
-    //__int16 field_FE_pad;
-    int field_100_alive_timer;
+    BYTE** field_F4_ppRes;
+    ParticleBurst_Item* field_F8_pRes;
+    __int16 field_FC_count;
+    __int16 field_FE_pad;
+    int field_100_timer;
     __int16 field_104_type;
-    __int16 field_106_count2;
+    __int16 field_106_count;
 };
 ALIVE_ASSERT_SIZEOF(ParticleBurst, 0x108);
