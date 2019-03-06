16898027ad9b20d6c1181f9265687fffd9b054ae
paul <Paul@paul.com>
2019-03-06 11:46:12 +0000

    some VS2017 built in cmake fixes, PCH and $ in post build/install events still breaks it

--- options.cmake
+++ options.cmake
@@ -11,4 +11,4 @@ option(USE_SDL2 "Use SDL2 instead of Win32 APIs." ON)
 option(USE_SDL2_SOUND "Use SDL2 for audio." ON)
 option(ORIGINAL_PS1_BEHAVIOR "Fixes bugs in the PSX Emu layer / Gameplay to match PS1 version of the game." ON)
 
-CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/../Source/config.h.in ${CMAKE_CURRENT_SOURCE_DIR}/config.h)
+CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/Source/config.h.in ${CMAKE_CURRENT_SOURCE_DIR}/Source/AliveLib/config.h)
