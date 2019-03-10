35f76d69c273e6ff7523c37405a870ae9cda8823
paul <Paul@paul.com>
2019-03-10 01:49:24 +0000

    impl Class_544534::ctor_416390 impl Class_544534::vUpdate_416570 impl Class_544534::vScreenChanged_416720 impl Class_544534::dtor_416550 impl Class_544534::vdtor_416520

--- Source/AliveLib/ChantSuppressor.cpp
+++ Source/AliveLib/ChantSuppressor.cpp
@@ -5,6 +5,113 @@
 #include "Gibs.hpp"
 #include "Function.hpp"
 
+class Class_544534 : public BaseAnimatedWithPhysicsGameObject
+{
+public:
+    EXPORT Class_544534* ctor_416390(FP xpos, FP ypos, FP scale)
+    {
+        BaseAnimatedWithPhysicsGameObject_ctor_424930(0);
+        SetVTable(this, 0x544534);
+        field_4_typeId = Types::eType_22;
+
+        BYTE** ppRes = Add_Resource_4DC130(ResourceManager::Resource_Animation, ResourceID::kSparksResID);
+        Animation_Init_424E10(808, 19, 11, ppRes, 1, 1);
+        
+        field_DC_bApplyShadows &= ~1u;
+
+        field_20_animation.field_C_render_layer = 37;
+        field_20_animation.field_B_render_mode = 1;
+
+        field_20_animation.field_A_b = 80;
+        field_20_animation.field_9_g = 80;
+        field_20_animation.field_8_r = 80;
+
+        field_CC_sprite_scale = scale * (((FP_FromInteger(Math_NextRandom() % 6)) / FP_FromInteger(10)) + FP_FromDouble(0.7));
+        field_FA_16_random = Math_RandomRange_496AB0(0, 16);
+
+        field_B8_xpos = xpos;
+        field_BC_ypos = ypos;
+
+        field_C4_velx = FP_FromInteger(Math_RandomRange_496AB0(-8, 8));
+        field_C8_vely = FP_FromInteger(Math_RandomRange_496AB0(-6, -3));
+        return this;
+    }
+
+    virtual BaseGameObject* VDestructor(signed int flags) override
+    {
+        return vdtor_416520(flags);
+    }
+
+    virtual void VUpdate() override
+    {
+        vUpdate_416570();
+    }
+
+    virtual void VScreenChanged() override
+    {
+        vScreenChanged_416720();
+    }
+
+private:
+
+    EXPORT void vUpdate_416570()
+    {
+        if (field_FA_16_random > 0)
+        {
+            field_FA_16_random--;
+        }
+
+        if (field_FA_16_random == 0)
+        {
+            field_20_animation.Set_Animation_Data_409C80(808, 0);
+            field_FA_16_random = -1;
+        }
+
+        field_C8_vely += FP_FromDouble(0.8);
+
+        field_C4_velx *= FP_FromDouble(0.9);
+        field_C8_vely *= FP_FromDouble(0.9);
+
+        field_C4_velx += FP_FromInteger(Math_NextRandom() - 127) / FP_FromInteger(64);
+        field_C8_vely += FP_FromInteger(Math_NextRandom() - 127) / FP_FromInteger(64);
+
+        field_BC_ypos += field_C8_vely;
+        field_B8_xpos += field_C4_velx;
+
+        if (!gMap_5C3030.Is_Point_In_Current_Camera_4810D0(field_C2_lvl_number, field_C0_path_number, field_B8_xpos, field_BC_ypos, 0))
+        {
+            field_6_flags.Set(BaseGameObject::eDead);
+        }
+    }
+
+    EXPORT void vScreenChanged_416720()
+    {
+        field_6_flags.Set(BaseGameObject::eDead);
+    }
+
+    EXPORT void dtor_416550()
+    {
+        BaseAnimatedWithPhysicsGameObject_dtor_424AD0();
+    }
+
+    EXPORT Class_544534* vdtor_416520(signed int flags)
+    {
+        dtor_416550();
+        if (flags & 1)
+        {
+            Mem_Free_495540(this);
+        }
+        return this;
+    }
+
+private:
+    int field_E4_not_used[4];
+    int field_F4_not_used1;
+    __int16 field_F8_not_used2;
+    __int16 field_FA_16_random;
+};
+ALIVE_ASSERT_SIZEOF(Class_544534, 0xFC);
+
 const TintEntry sChantOrbTints_55C1EC[18] =
 {
     { 1u, 127u, 127u, 127u },
