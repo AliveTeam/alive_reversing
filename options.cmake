a99acca3231307f5b14b77f6be57dc57311ed5f1
Michael Grima <mlgthatsme@hotmail.com.au>
2019-04-07 18:53:01 +1000

    Remove mobile option from cmake, let android build env deal with that

--- options.cmake
+++ options.cmake
@@ -11,6 +11,5 @@ option(USE_SDL2 "Use SDL2 instead of Win32 APIs." ON)
 option(USE_SDL2_SOUND "Use SDL2 for audio." ON)
 option(USE_SDL2_IO "Use SDL2 for all File/Stream IO." ON)
 option(ORIGINAL_PS1_BEHAVIOR "Fixes bugs in the PSX Emu layer / Gameplay to match PS1 version of the game." ON)
-option(MOBILE "Enable mobile specific features. (Touch Screen Controls, etc)" OFF)
 
 CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/Source/config.h.in ${CMAKE_CURRENT_SOURCE_DIR}/Source/AliveLib/config.h)
