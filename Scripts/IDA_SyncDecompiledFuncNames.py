c85590c2ebfccccf18aee28b155cc31264b083bf
paul <Paul@paul.com>
2019-06-12 23:01:15 +0100

    script to auto sync C++ -> IDA function names

---
+++ Scripts/IDA_SyncDecompiledFuncNames.py
@@ -0,0 +1,46 @@
+from idautils import *
+from idaapi import *
+from idc import *
+
+def ida_func_exists(function_address):
+    for segment in Segments():
+        # get all functions
+        for function_ea in Functions(segment, SegEnd(segment)):
+            if function_address == function_ea:
+                return True
+    return False
+
+def LineToInt(line):
+    line = line.strip();
+    return long(line)
+
+def EnsureKey(address, dict):
+    if not dict.has_key(address):
+        dict[address] = ""
+
+# line will be a string of the form 4393888=Fleech::AI_0_430BA0
+def AddFunction(line, dict):
+    address = LineToInt(line.split('=')[0])
+    EnsureKey(address, dict)
+    dict[address] = line.split('=')[1].strip()
+
+def sync_function_name(adddress, name):
+    if ida_func_exists(adddress):
+        currentName = GetFunctionName(adddress)
+        if currentName != name:
+            print "rename: " + currentName + " to " + name
+            idc.MakeNameEx(adddress, name, idc.SN_NOWARN)
+ 
+def main():
+    functionDataDict = {}
+
+    with open('C:\\GOG Games\\Abes Exoddus\\function_names.txt', 'r') as f:
+        for line in f:
+            AddFunction(line, functionDataDict)
+
+    for address in functionDataDict.iterkeys():
+        data = functionDataDict[address]
+        sync_function_name(address, data)
+
+if __name__ == '__main__':
+    main()
