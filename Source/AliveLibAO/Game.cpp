e4d382d7bb377463f6f0c26071ac6bc3c4c5da1f
Paul <github@paulsapps.com>
2020-04-29 18:54:16 +0100

    failed font rendering attempts

--- Source/AliveLibAO/Game.cpp
+++ Source/AliveLibAO/Game.cpp
@@ -38,6 +38,8 @@ EXPORT void Mem_Free_450770(void*)
 }
 
 ALIVE_VAR(1, 0x9F2DF0, class DynamicArray*, gBaseGameObject_list_9F2DF0, nullptr);
+ALIVE_VAR(1, 0x504618, class DynamicArray*, gObjList_drawables_504618, nullptr);
+
 
 EXPORT void CC Remove_Item_417350(void*)
 {
@@ -151,7 +153,7 @@ public:
         return 1;
     }
 
-protected:
+//protected:
 
     EXPORT __int16 Remove_Item_404520(void *item)
     {
@@ -237,9 +239,150 @@ const static std::map<DWORD, std::string> sVTableToName =
     // 
 };
 
+ALIVE_VAR(1, 0xAC6420, int*, psxemu_videomem_AC6420, nullptr);
+
+class Bmp
+{
+public:
+    // 0x00ac6420
+    // 8
+    // 16
+    // 0x00ac6420 fff7
+    // 0x00ac6420 1
+    // str
+    EXPORT static int CC DrawString_48FB90(int* /*pBmp*/, int /*x*/, int /*y*/, int /*colour*/, int /*backColour*/, const char* /*lpString*/)
+    {
+        NOT_IMPLEMENTED();
+        return 0;
+    }
+};
+
+class FontContext
+{
+public:
+    EXPORT __int16 LoadFontType_41C040(int)
+    {
+        NOT_IMPLEMENTED();
+    }
+
+
+    EXPORT void dtor_41C110()
+    {
+        NOT_IMPLEMENTED();
+    }
+
+
+    __int16 field_0;
+    __int16 field_2;
+    int field_4;
+    int field_8;
+    __int16 field_C;
+    __int16 field_E;
+};
+
+ALIVE_VAR(1, 0x4FFD68, FontContext, sFontContext_4FFD68, {});
+
+class AliveFont
+{
+public:
+    EXPORT AliveFont* ctor_41C170(int /*a2*/, BYTE* /*pPal*/, FontContext* /*pContext*/)
+    {
+        NOT_IMPLEMENTED();
+    }
+
+    EXPORT unsigned int MeasureWidth_41C2B0(const char* /*pStr*/)
+    {
+        NOT_IMPLEMENTED();
+        return 0;
+    }
+
+    EXPORT int DrawString_41C360(int** , const char *, int, __int16 , int , int , int , int , char , char , char , int , signed int , int , int )
+    {
+        NOT_IMPLEMENTED();
+        return 0;
+    }
+
+    EXPORT void dtor_41C130()
+    {
+        NOT_IMPLEMENTED();
+    }
+
+
+    int field_0;
+    int field_4;
+    int field_8;
+    int field_C;
+    int field_10;
+    int field_14;
+    int field_18;
+    int field_1C;
+    int field_20;
+    int field_24;
+    __int16 field_28;
+    __int16 field_2A;
+    int field_2C;
+    int field_30;
+    FontContext *field_34_pFontContext;
+};
+
+char sStatsSignFontPalette_4CD570[32] =
+{
+  '\0',
+  '\0',
+  '\x01',
+  '€',
+  '\x01',
+  '„',
+  ' ',
+  '„',
+  '!',
+  '€',
+  ' ',
+  '„',
+  '!',
+  '„',
+  'e',
+  'Î',
+  'e',
+  'Œ',
+  'Œ',
+  '±',
+  '\x13',
+  '”',
+  'd',
+  'Î',
+  'e',
+  'Î',
+  '×',
+  '˜',
+  '\x14',
+  '¡',
+  '\x18',
+  'Ø'
+};
+
+
+// TODO: Wrong values
+ BYTE pal_4D0090[32] = {
+    0x00, 0x00, 0x21, 0x84, 0x42, 0x88, 0x63, 0x8C, 0x84, 0x90,
+    0xA5, 0x14, 0xE7, 0x1C, 0x08, 0x21, 0x29, 0x25, 0x4A, 0x29,
+    0x6B, 0x2D, 0x8C, 0x31, 0xAD, 0x35, 0xEF, 0x3D, 0x10, 0x42,
+    0x73, 0x4E };
+
+EXPORT void ResourceManager_Request_446C90(const char *, int , int , __int16 , int )
+{
+    NOT_IMPLEMENTED();
+}
+
 class ObjectDumper : public BaseGameObject
 {
 public:
+
+    AliveFont mFont;
+    //FontContext mFontContext;
+    int mCounter = 0;
+    bool mLoaded = false;
+
     ObjectDumper()
     {
         DisableVTableHack disableHack;
@@ -248,9 +391,14 @@ public:
         LOG_INFO(field_6_flags);
 
         field_6_flags |= 0x300;
+        field_6_flags |= 0x8;
         field_8_update_delay = 0;
 
+        gObjList_drawables_504618->Push_Back_404450(this);
+
         gObjDumper = this;
+
+
     }
 
 
@@ -262,6 +410,11 @@ public:
     virtual BaseGameObject* VDestructor(signed int flags) override
     {
         gObjDumper = nullptr;
+        gObjList_drawables_504618->Remove_Item_404520(this);
+
+        mFont.dtor_41C130();
+        //mFontContext.dtor_41C110();
+
         dtor_487DF0();
         if (flags & 1)
         {
@@ -272,13 +425,54 @@ public:
 
     virtual void VUpdate() override
     {
-        DumpObjects();
+        //DumpObjects();
 
+        mCounter++;
+        if (mCounter > 150 && !mLoaded)
+        {
+
+            //ResourceManager_Request_446C90("LCDFONT.FNT", 'tnoF', 2, 1, 0);
+
+           // mFontContext.LoadFontType_41C040(2);
+            mFont.ctor_41C170(240, pal_4D0090, &sFontContext_4FFD68);
+
+            mLoaded = true;
+        }
     }
 
     virtual void VScreenChanged() override
     {
         //LOG_INFO("Don't kill me!");
+    }
+
+    virtual void VRender(int** ppOt)
+    {
+        if (mLoaded)
+        {
+            const char* text = "Hello world";
+
+            const auto v3 = mFont.MeasureWidth_41C2B0(text);
+
+            mFont.DrawString_41C360(
+                ppOt,
+                text,
+                50,
+                50,
+                1,
+                1,
+                0,
+                22,
+                127,
+                127,
+                127,
+                0,
+                0x10000,
+                20,
+                500);
+        }
+       
+       // Bmp::DrawString_48FB90(psxemu_videomem_AC6420, 20, 20, 0xfff7, 0x01, "Mouze VS Ultra");
+       
     }
 
     virtual void VUnknown() override
