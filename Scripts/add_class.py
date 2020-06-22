e6a3c82952b23689ff64c29c00f6fe52fed4b65e
Paul <github@paulsapps.com>
2020-06-22 01:34:48 +0100

    add script working

--- Scripts/add_class.py
+++ Scripts/add_class.py
@@ -1,6 +1,7 @@
 import os
 print os.getcwd()
 
+className = "test"
 
 cmakePath = os.getcwd() + "\..\Source\AliveLibAO\CMakeLists.txt"
 print(cmakePath)
@@ -20,9 +21,36 @@ if pos == -1:
 before = content[:pos+17]
 after = content[pos+18:]
 
-print(before)
-print("   test.hpp")
-print("   test.cpp")
-print(after)
+f = open(cmakePath, "w")
+f.write(before + "\n")
+f.write("   " + className + ".hpp\n")
+f.write("   " + className + ".cpp\n")
+f.write(after)
+f.close()
 
+h = open(os.getcwd() + "\\..\\Source\\AliveLibAO\\" + className + ".hpp", "w")
+h.write("""#pragma once
+
+#include "FunctionFwd.hpp"
+
+START_NS_AO
+
+END_NS_AO
+
+""")
+h.close()
+
+c = open(os.getcwd() + "\\..\\Source\\AliveLibAO\\" + className + ".cpp", "w")
+c_data = """#include "stdafx_ao.h"
+#include "Function.hpp"
+#include "replace_me.hpp"
+
+START_NS_AO
+
+END_NS_AO
+
+"""
+c.write(c_data.replace("replace_me.hpp", className + ".hpp"))
+c.close()
+         
 print("cheers lads")
