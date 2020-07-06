5039183b55d9a3419cd0ef25dac0e577c79abfad
Paul <github@paulsapps.com>
2020-07-06 15:28:50 +0100

    stub security door

---
+++ Source/AliveLibAO/SecurityDoor.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "FunctionFwd.hpp"
+#include "BaseAnimatedWithPhysicsGameObject.hpp"
+#include "Map.hpp"
+
+START_NS_AO
+
+struct Path_SecurityDoor : public Path_TLV
+{
+    __int16 field_18_scale;
+    __int16 field_1A_id;
+    __int16 field_1C_code_1;
+    __int16 field_1E_code2;
+    __int16 field_20_xpos;
+    __int16 field_22_ypos;
+};
+ALIVE_ASSERT_SIZEOF(Path_SecurityDoor, 0x24);
+
+class SecurityDoor : public BaseAnimatedWithPhysicsGameObject
+{
+public:
+    EXPORT SecurityDoor* ctor_461840(Path_SecurityDoor* pTlv, int tlvInfo);
+
+    EXPORT BaseGameObject* dtor_4619C0();
+
+    virtual BaseGameObject* VDestructor(signed int flags) override;
+
+    EXPORT SecurityDoor* vdtor_461F90(signed int flags);
+
+    virtual void VScreenChanged() override;
+
+    EXPORT void VScreenChanged_461F80();
+
+    int field_D4[4];
+    int field_E4_tlvInfo;
+    __int16 field_E8_state;
+    __int16 field_EA_id;
+    int field_EC_code_converted;
+    __int16 field_F0_code_len;
+    __int16 field_F2_padding;
+    int field_F4_event_idx;
+    BYTE field_F8_stru[16];
+    __int16 field_108_max_idx;
+    __int16 field_10A_event_idx;
+    PSX_Point field_10C_top_left;
+    PSX_Point field_110_bottom_right;
+    int field_114_timer;
+    __int16 field_118_max_idx;
+    __int16 field_11A_unused;
+};
+ALIVE_ASSERT_SIZEOF(SecurityDoor, 0x11C);
+
+END_NS_AO
+
