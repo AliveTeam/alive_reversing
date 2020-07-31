863d16f7eb2c51c1fbc53d44a4b8a53dbb298f12
Paul <github@paulsapps.com>
2020-07-31 03:13:12 +0100

    meat stub

--- Source/AliveLibAO/Meat.cpp
+++ Source/AliveLibAO/Meat.cpp
@@ -4,6 +4,8 @@
 #include "ResourceManager.hpp"
 #include "stdlib.hpp"
 #include "Shadow.hpp"
+#include "Game.hpp"
+#include "ThrowableArray.hpp"
 
 START_NS_AO
 
@@ -94,6 +96,125 @@ void MeatSack::VScreenChanged()
 void MeatSack::VScreenChanged_439540()
 {
     field_6_flags.Set(BaseGameObject::eDead_Bit3);
+}
+
+Meat* Meat::ctor_438550(FP xpos, FP ypos, __int16 count)
+{
+    ctor_401090();
+    SetVTable(this, 0x4BB8B0);
+
+    field_10E_bDead = 0;
+
+    field_4_typeId = Types::eMeat_54;
+
+    BYTE** ppRes = ResourceManager::GetLoadedResource_4554F0(ResourceManager::Resource_Animation, 2023, 1, 0);
+    Animation_Init_417FD0(
+        500,
+        18,
+        9,
+        ppRes,
+        1);
+
+    field_A8_xpos = xpos;
+    field_AC_ypos = ypos;
+
+    field_114_xpos = xpos;
+    field_118_ypos = ypos;
+
+    field_B4_velx = FP_FromInteger(0);
+    field_B8_vely = FP_FromInteger(0);
+    field_11C_timer = 0;
+    field_6_flags.Clear(Options::eInteractive_Bit8);
+
+    field_10_anim.field_4_flags.Clear(AnimFlags::eBit3_Render);
+    field_10_anim.field_4_flags.Clear(AnimFlags::eBit15_bSemiTrans);
+
+    field_120_deadtimer = gnFrameCount_507670 + 600;
+    field_124_pLine = 0;
+    field_10C_count = count;
+    field_110_state = 0;
+
+    field_D0_pShadow = ao_new<Shadow>();
+    if (field_D0_pShadow)
+    {
+        field_D0_pShadow->ctor_461FB0();
+    }
+
+    return this;
+}
+
+BaseGameObject* Meat::dtor_438660()
+{
+    SetVTable(this, 0x4BB8B0);
+    if (!field_10E_bDead)
+    {
+        if (gpThrowableArray_50E26C)
+        {
+            gpThrowableArray_50E26C->Remove_4540D0(field_10C_count >= 1u ? field_10C_count : 1);
+        }
+    }
+    return dtor_401000();
+}
+
+BaseGameObject* Meat::VDestructor(signed int flags)
+{
+    return Vdtor_4390D0(flags);
+}
+
+Meat* Meat::Vdtor_4390D0(signed int flags)
+{
+    dtor_438660();
+    if (flags & 1)
+    {
+        ao_delete_free_447540(this);
+    }
+    return this;
+}
+
+void Meat::VScreenChanged()
+{
+    VScreenChanged_438E70();
+}
+
+void Meat::VScreenChanged_438E70()
+{
+    if (gMap_507BA8.field_2_current_path != gMap_507BA8.field_C_path ||
+        gMap_507BA8.field_0_current_level != gMap_507BA8.field_A_level)
+    {
+        field_6_flags.Set(BaseGameObject::eDead_Bit3);
+    }
+}
+
+void Meat::VThrow(FP velX, FP velY)
+{
+    VThrow_4386E0(velX, velY);
+}
+
+void Meat::VThrow_4386E0(FP velX, FP velY)
+{
+    field_10_anim.field_4_flags.Set(AnimFlags::eBit3_Render);
+
+    field_B4_velx = velX;
+    field_B8_vely = velY;
+
+    if (field_10C_count == 0)
+    {
+        field_110_state = 2;
+    }
+    else
+    {
+        field_110_state = 1;
+    }
+}
+
+__int16 Meat::VCanThrow()
+{
+    return VCanThrow_4390B0();
+}
+
+__int16 Meat::VCanThrow_4390B0()
+{
+    return field_110_state == 2;
 }
 
 END_NS_AO
