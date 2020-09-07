61161b9a209b140f53ede554e0c41e3a89368f8a
Paul <github@paulsapps.com>
2020-09-07 01:43:19 +0100

    fix build after taking latest upstream master

--- Source/AliveLibAO/Slig.cpp
+++ Source/AliveLibAO/Slig.cpp
@@ -1310,13 +1310,7 @@ signed __int16 Slig::MainMovement_467020()
             }
         }
 
-        field_12C = 0;
-        field_B4_velx = FP_FromInteger(0);
-        field_B8_vely = FP_FromInteger(0);
-        field_FC_current_motion = eSligStates::State_0_StandIdle_467640;
-        field_126_input = 0;
-        field_128_timer = Math_RandomRange_450F20(0, 60) + gnFrameCount_507670 + 120;
-        MapFollowMe_401D30(1);
+        ToStand();
         return 0;
     }
 
