826a133c5ef137db57fea0a577c3cda6c5ff943d
Paul <github@paulsapps.com>
2020-09-13 16:21:29 +0100

    impl Paramite::State_16_PostHiss_44D440

--- Source/AliveLibAO/Paramite.cpp
+++ Source/AliveLibAO/Paramite.cpp
@@ -3477,7 +3477,30 @@ void Paramite::State_15_Hiss_44D300()
 
 void Paramite::State_16_PostHiss_44D440()
 {
-    NOT_IMPLEMENTED();
+    PSX_RECT abeRect = {};
+    sActiveHero_507678->VGetBoundingRect(&abeRect, 1);
+
+    PSX_RECT rect = {};
+    VGetBoundingRect(&rect, 1);
+
+    if (abeRect.x <= rect.w
+        && abeRect.w >= rect.x
+        && abeRect.h >= rect.y
+        && abeRect.y <= rect.h)
+    {
+        if (VIsObj_GettingNear_On_X(sActiveHero_507678))
+        {
+            if (field_BC_sprite_scale == sActiveHero_507678->field_BC_sprite_scale)
+            {
+                sActiveHero_507678->VTakeDamage(this);
+            }
+        }
+    }
+
+    if (field_10_anim.field_4_flags.Get(AnimFlags::eBit18_IsLastFrame))
+    {
+        field_FC_current_motion = eParamiteStates::State_14_PreHiss_44D170;
+    }
 }
 
 void Paramite::State_17_GameSpeakEnd_44D4F0()
