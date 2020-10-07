e36847cadc655fd96f6acf224ee8b5be16f35f71
Paul <github@paulsapps.com>
2020-10-07 20:59:15 +0100

    impl Collisions::RayCast_40C410

--- Source/AliveLibAO/Collisions.cpp
+++ Source/AliveLibAO/Collisions.cpp
@@ -5,6 +5,10 @@
 #include "Psx_common.hpp"
 #include "PathData.hpp"
 
+// windows.h pollution fix
+#undef min
+#undef max
+
 START_NS_AO
 
 ALIVE_VAR(1, 0x504C6C, Collisions*, sCollisions_DArray_504C6C, nullptr);
@@ -79,10 +83,254 @@ PathLine* Collisions::Add_Dynamic_Collision_Line_40C8A0(__int16 x1, __int16 y1, 
     return pAddedLine;
 }
 
-signed __int16 Collisions::RayCast_40C410(FP /*X1_16_16*/, FP /*Y1_16_16*/, FP /*X2_16_16*/, FP /*Y2_16_16*/, PathLine** /*ppLine*/, FP* /*hitX*/, FP* /*hitY*/, unsigned int /*modeMask*/)
+
+// 24:8 fixed type.. I guess 16:16 wasn't good enough for collision detection
+class Fixed_24_8
 {
-    NOT_IMPLEMENTED();
-    return 0;
+public:
+    Fixed_24_8()
+        : fpValue(0)
+    {
+
+    }
+
+    explicit Fixed_24_8(int val)
+    {
+        fpValue = val * 256;
+    }
+
+    explicit Fixed_24_8(FP fixed16_16)
+    {
+        fpValue = fixed16_16.fpValue / 256;
+    }
+
+    int GetExponent() const
+    {
+        return fpValue / 256;
+    }
+
+    Fixed_24_8 Abs() const
+    {
+        Fixed_24_8 r;
+        if (fpValue > 0)
+        {
+            r.fpValue = fpValue;
+        }
+        else
+        {
+            r.fpValue = -fpValue;
+        }
+        return r;
+    }
+
+    int fpValue;
+};
+
+inline bool operator > (const Fixed_24_8& lhs, const Fixed_24_8& rhs)
+{
+    return lhs.fpValue > rhs.fpValue;
+}
+
+inline bool operator < (const Fixed_24_8& lhs, const Fixed_24_8& rhs)
+{
+    return lhs.fpValue < rhs.fpValue;
+}
+
+inline Fixed_24_8 operator - (const Fixed_24_8& lhs, const Fixed_24_8& rhs)
+{
+    Fixed_24_8 r;
+    r.fpValue = lhs.fpValue - rhs.fpValue;
+    return r;
+}
+
+inline Fixed_24_8 operator + (const Fixed_24_8& lhs, const Fixed_24_8& rhs)
+{
+    Fixed_24_8 r;
+    r.fpValue = lhs.fpValue + rhs.fpValue;
+    return r;
+}
+
+inline Fixed_24_8 operator * (const Fixed_24_8& lhs, const Fixed_24_8& rhs)
+{
+    // Whenever we see alldiv/allmul __PAIR__ and odd 64bit math its likely something like the following.
+
+    // Multiply as 64bit numbers to get more precision in the results
+    unsigned long long left = lhs.fpValue;
+    unsigned long long right = rhs.fpValue;
+    unsigned long long mult = (left * right) / 256;
+
+    Fixed_24_8 r;
+    r.fpValue = static_cast<int>(mult);
+    return r;
+}
+
+inline Fixed_24_8 operator / (const Fixed_24_8& lhs, const Fixed_24_8& rhs)
+{
+    Fixed_24_8 r;
+    r.fpValue = lhs.fpValue / (rhs.GetExponent());
+    return r;
+}
+
+signed __int16 Collisions::RayCast_40C410(FP X1_16_16, FP Y1_16_16, FP X2_16_16, FP Y2_16_16, PathLine** ppLine, FP* hitX, FP* hitY, unsigned int modeMask)
+{
+    // NOTE: The local static k256_dword_5BC034 is omitted since its actually just a constant of 256
+
+    // The following was a huge help in figuring this out:
+    // https://stackoverflow.com/questions/35473936/find-whether-two-line-segments-intersect-or-not-in-c
+
+    Fixed_24_8 x1(X1_16_16);
+    Fixed_24_8 x2(X2_16_16);
+    Fixed_24_8 y1(Y1_16_16);
+    Fixed_24_8 y2(Y2_16_16);
+
+    int minX = std::min(x1, x2).GetExponent();
+    int maxX = std::max(x1, x2).GetExponent();
+
+    int minY = std::min(y1, y2).GetExponent();
+    int maxY = std::max(y1, y2).GetExponent();
+
+    Fixed_24_8 xDiff = x2 - x1;
+    Fixed_24_8 yDiff = y2 - y1;
+
+    Fixed_24_8 nearestMatch(2); // 512
+
+    Fixed_24_8 epslion;
+    epslion.fpValue = 256; // 0.99
+
+    PathLine* pNearestMatch = nullptr;
+
+    for (int i = 0; i < field_C_max_count; i++)
+    {
+        PathLine* pLine = &field_0_pArray[i];
+        if (!(1 << (pLine->field_8_type % 32) & modeMask))
+        {
+            // Not a match on type
+            continue;
+        }
+
+        if (std::min(pLine->field_0_rect.x, pLine->field_0_rect.w) > maxX)
+        {
+            continue;
+        }
+
+        if (std::max(pLine->field_0_rect.x, pLine->field_0_rect.w) < minX)
+        {
+            continue;
+        }
+
+        if (std::min(pLine->field_0_rect.y, pLine->field_0_rect.h) > maxY)
+        {
+            continue;
+        }
+
+        if (std::max(pLine->field_0_rect.y, pLine->field_0_rect.h) < minY)
+        {
+            continue;
+        }
+
+        Fixed_24_8 xDiffCurrent(pLine->field_0_rect.w - pLine->field_0_rect.x);
+        Fixed_24_8 yDiffCurrent(pLine->field_0_rect.h - pLine->field_0_rect.y);
+
+        Fixed_24_8 det = (xDiffCurrent * yDiff) - (xDiff * yDiffCurrent);
+        if (det.Abs() < epslion)
+        {
+            continue;
+        }
+
+        Fixed_24_8 unknown1 =
+            xDiffCurrent * (Fixed_24_8(pLine->field_0_rect.y) - y1) -
+            yDiffCurrent * (Fixed_24_8(pLine->field_0_rect.x) - x1);
+
+        if (det > Fixed_24_8(0))
+        {
+            if (unknown1 < Fixed_24_8(0))
+            {
+                continue;
+            }
+
+            if (unknown1 > det)
+            {
+                continue;
+            }
+        }
+        else
+        {
+            if (unknown1 > Fixed_24_8(0))
+            {
+                continue;
+            }
+
+            if (unknown1 < det)
+            {
+                continue;
+            }
+        }
+
+        Fixed_24_8 unknown2 =
+            xDiff * (Fixed_24_8(pLine->field_0_rect.y) - y1) -
+            yDiff * (Fixed_24_8(pLine->field_0_rect.x) - x1);
+
+        if (det > Fixed_24_8(0))
+        {
+            if (unknown2 < Fixed_24_8(0))
+            {
+                continue;
+            }
+
+            if (unknown2 > det)
+            {
+                continue;
+            }
+        }
+        else
+        {
+            if (unknown2 > Fixed_24_8(0))
+            {
+                continue;
+            }
+
+            if (unknown2 < det)
+            {
+                continue;
+            }
+        }
+
+        unknown1 = unknown1 / det;
+
+        if (unknown1 < nearestMatch)
+        {
+            nearestMatch = unknown1;
+            pNearestMatch = pLine;
+        }
+
+    }
+
+    if (nearestMatch < Fixed_24_8(2))
+    {
+        // TODO: This needs cleaning up
+        int xh = x1.fpValue + ((xDiff.fpValue * nearestMatch.fpValue) / 256);
+        int yh = y1.fpValue + ((yDiff.fpValue * nearestMatch.fpValue) / 256);
+
+        *hitX = FP_FromRaw(xh << 8);
+        *hitY = FP_FromRaw(yh << 8);
+
+        *ppLine = pNearestMatch;
+
+#if DEVELOPER_MODE
+        DebugAddRaycast({ X1_16_16,Y1_16_16,X2_16_16,Y2_16_16,*hitX,*hitY, *ppLine, modeMask });
+#endif
+
+        return TRUE;
+    }
+
+    *ppLine = nullptr;
+
+
+#if DEVELOPER_MODE
+    DebugAddRaycast({ X1_16_16,Y1_16_16,X2_16_16,Y2_16_16,*hitX,*hitY, *ppLine, modeMask });
+#endif
+
+    return FALSE;
 }
 
 const int kNearLineTollerance = 8;
