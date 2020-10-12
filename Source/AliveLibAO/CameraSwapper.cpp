f85a574b50487df3e108d4c3e006905c69e7ff8e
Paul <github@paulsapps.com>
2020-10-12 12:33:12 +0100

    impl CameraSwapper::Init_48C830

--- Source/AliveLibAO/CameraSwapper.cpp
+++ Source/AliveLibAO/CameraSwapper.cpp
@@ -9,6 +9,10 @@
 #include "ScreenClipper.hpp"
 #include "MusicController.hpp"
 #include "BackgroundMusic.hpp"
+#include "ScreenManager.hpp"
+#include "PsxDisplay.hpp"
+#include "Sfx.hpp"
+#include "Sys_common.hpp"
 
 START_NS_AO
 
@@ -124,12 +128,6 @@ CameraSwapper* CameraSwapper::ctor_48C7A0(BYTE** ppBits, CameraSwapEffects chang
     return this;
 }
 
-
-void CameraSwapper::Init_48C830(BYTE** /*ppCamRes*/, CameraSwapEffects /*changeEffect*/)
-{
-    NOT_IMPLEMENTED();
-}
-
 BaseGameObject* CameraSwapper::dtor_48CE00()
 {
     SetVTable(this, 0x4BD7C8);
@@ -157,6 +155,215 @@ BaseGameObject* CameraSwapper::dtor_48CE00()
 void CameraSwapper::VUpdate()
 {
     VUpdate_48CEA0();
+}
+
+void CameraSwapper::Init_48C830(BYTE** ppCamRes, CameraSwapEffects changeEffect)
+{
+    field_6_flags.Set(Options::eUpdateDuringCamSwap_Bit10);
+
+    field_4_typeId = Types::eCameraSwapper_102;
+
+    field_24_pSubObject = nullptr;
+
+    if (changeEffect == CameraSwapEffects::eEffect5_1_FMV || changeEffect == CameraSwapEffects::eEffect9_2_FMV || changeEffect == CameraSwapEffects::eEffect10_3_FMV)
+    {
+        field_20_ppCamRes = ppCamRes;
+    }
+    else
+    {
+        pScreenManager_4FF7C8->DecompressToVRam_407110(reinterpret_cast<WORD**>(ppCamRes));
+    }
+
+    sNumCamSwappers_507668++;
+
+    if (sNumCamSwappers_507668 != 1)
+    {
+        field_6_flags.Clear(BaseGameObject::eUpdatable_Bit2);
+        field_6_flags.Set(BaseGameObject::eListAddFailed_Bit1);
+        field_6_flags.Set(BaseGameObject::eDead_Bit3);
+
+        // There can only be 1 active at a time
+        return;
+    }
+
+    field_28_changeEffect = changeEffect;
+
+    PSX_Point xy = {};
+    PSX_Point wh = {};
+
+    switch (field_28_changeEffect)
+    {
+    case CameraSwapEffects::eEffect0_InstantChange:
+        pScreenManager_4FF7C8->InvalidateRect_Layer3_406F20(0, 0, 640, 240);
+        field_6_flags.Set(BaseGameObject::eDead_Bit3);
+        field_24_pSubObject = nullptr;
+        break;
+
+    case CameraSwapEffects::eEffect1_LeftToRight:
+        field_46_slice_width = 48; // 384 / 8 ?
+        field_2C_slices_per_tick = -1;
+        field_2E_total_slices = gPsxDisplay_504C78.field_0_width / field_46_slice_width;
+        field_2A_current_slice = field_2E_total_slices;
+      
+        xy.field_2_y = 0;
+        xy.field_0_x = gPsxDisplay_504C78.field_0_width;
+
+        wh.field_0_x = gPsxDisplay_504C78.field_0_width;
+        wh.field_2_y =  gPsxDisplay_504C78.field_2_height;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, wh, 0);
+        break;
+
+    case CameraSwapEffects::eEffect2_RightToLeft:
+        field_46_slice_width = 48;
+        field_2C_slices_per_tick = 1;
+        field_2E_total_slices = gPsxDisplay_504C78.field_0_width / field_46_slice_width;
+        field_2A_current_slice = -1;
+
+        xy.field_2_y = 0;
+        xy.field_0_x = 0;
+
+        wh.field_0_x = 0;
+        wh.field_2_y = gPsxDisplay_504C78.field_2_height;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, wh, 0);
+        break;
+
+    case CameraSwapEffects::eEffect3_TopToBottom:
+        field_46_slice_width = 30; // 240 / 8 ?
+        field_2C_slices_per_tick = -1;
+        field_2E_total_slices =  gPsxDisplay_504C78.field_2_height / field_46_slice_width;
+        field_2A_current_slice = field_2E_total_slices;
+
+        xy.field_0_x = 0;
+        xy.field_2_y = gPsxDisplay_504C78.field_2_height;
+        
+        wh.field_0_x = gPsxDisplay_504C78.field_0_width;
+        wh.field_2_y = gPsxDisplay_504C78.field_2_height;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, wh, 0);
+        break;
+
+    case CameraSwapEffects::eEffect4_BottomToTop:
+        field_46_slice_width = 30; // 240 / 8 ?
+        field_2C_slices_per_tick = 1;
+        field_2E_total_slices = gPsxDisplay_504C78.field_2_height / field_46_slice_width;
+        field_2A_current_slice = -1;
+         
+        xy.field_0_x = 0;
+        xy.field_2_y = 0;
+
+        wh.field_0_x = gPsxDisplay_504C78.field_0_width;
+        wh.field_2_y = 0;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, wh, 0);
+        break;
+
+    case CameraSwapEffects::eEffect6_VerticalSplit:
+        field_46_slice_width = 24; // ??
+        field_2E_total_slices = (gPsxDisplay_504C78.field_0_width / 2) / field_46_slice_width;
+        field_2C_slices_per_tick = 1;
+        field_2A_current_slice = 0;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        xy.field_0_x = gPsxDisplay_504C78.field_0_width / 2;
+        xy.field_2_y = 0;
+
+        wh.field_0_x = gPsxDisplay_504C78.field_0_width / 2;
+        wh.field_2_y = gPsxDisplay_504C78.field_2_height;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, wh, 0);
+        break;
+
+
+    case CameraSwapEffects::eEffect7_HorizontalSplit:
+        field_46_slice_width = 15; // ??
+        field_2C_slices_per_tick = 1;
+        field_2E_total_slices = (gPsxDisplay_504C78.field_2_height / 2) / field_46_slice_width;
+        field_2A_current_slice = 0;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        xy.field_0_x = 0;
+        xy.field_2_y = gPsxDisplay_504C78.field_2_height / 2;
+
+        wh.field_0_x = gPsxDisplay_504C78.field_0_width;
+        wh.field_2_y = gPsxDisplay_504C78.field_2_height / 2;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, wh, 0);
+        break;
+
+    case CameraSwapEffects::eEffect8_BoxOut:
+    {
+        field_42_XSlices = 24; // ??
+        field_44_YSlices = 15; // ??
+
+        short xDiff = gPsxDisplay_504C78.field_0_width - field_3E_xpos_converted;
+        if (xDiff <= field_3E_xpos_converted)
+        {
+            xDiff = field_3E_xpos_converted;
+        }
+
+        short yDiff = gPsxDisplay_504C78.field_2_height - field_40_ypos_converted;
+        if (yDiff <= field_40_ypos_converted)
+        {
+            yDiff = field_40_ypos_converted;
+        }
+
+        short startingSlice = 0;
+        if ((xDiff / field_42_XSlices) <= (yDiff / field_44_YSlices))
+        {
+            startingSlice = yDiff / field_44_YSlices;
+        }
+        else
+        {
+            startingSlice = xDiff / field_42_XSlices;
+        }
+
+        field_2C_slices_per_tick = 1;
+        field_2E_total_slices = startingSlice + 1;
+        field_2A_current_slice = 0;
+
+        pScreenManager_4FF7C8->field_38 = 1;
+
+        xy.field_0_x = gPsxDisplay_504C78.field_0_width - 1;
+        xy.field_2_y = gPsxDisplay_504C78.field_2_height - 1;
+
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60(xy, { 1, 1 }, 0);
+
+        // "Whoosh" door sound effect
+        SFX_Play_43AD70(SoundEffect::IngameTransition_107, 127);
+        break;
+    }
+
+    case CameraSwapEffects::eEffect5_1_FMV:
+    case CameraSwapEffects::eEffect9_2_FMV:
+    case CameraSwapEffects::eEffect10_3_FMV:
+        pScreenManager_4FF7C8->field_38 = 1;
+        field_24_pSubObject = ao_new<ScreenClipper>();
+        field_24_pSubObject->ctor_40BD60({0, 0}, {1, 1}, 0);
+        field_2A_current_slice = 0;
+        break;
+
+    default:
+        ALIVE_FATAL("Unknown camera swap effect"); // Or one that isn't handled here
+    }
 }
 
 void CameraSwapper::VUpdate_48CEA0()
