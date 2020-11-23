05b7f94dcf88e131488b29c4ead40b4941b85016
Paul <github@paulsapps.com>
2020-11-23 13:52:44 +0000

    impl MainMenuTransition::VRender_436610

--- Source/AliveLibAO/MainMenu.cpp
+++ Source/AliveLibAO/MainMenu.cpp
@@ -413,6 +413,46 @@ void MainMenuFade::VRender_42A7A0(int** ppOt)
         pScreenManager_4FF7C8->field_2E_idx);
 }
 
+
+struct MainMenu_TransitionData
+{
+    __int16 field_0;
+    __int16 field_2;
+    __int16 field_4;
+    __int16 field_6;
+};
+ALIVE_ASSERT_SIZEOF(MainMenu_TransitionData, 0x8);
+
+const MainMenu_TransitionData stru_55C038[24] = // 3 x 8's ?
+{
+    { -8,       -8,         384, 1 },
+    { 0,       -32,         256, 1 },
+    { 8,       -8,          384, 1 },
+    { 32,       0,          256, 1 },
+    { 8,        8,          384, 1 },
+    { 0,        32,         256, 1 },
+    { -8,       8,          384, 1 },
+    { -32,      0,          256, 1 },
+
+    { -16,      -16,        256, 1 },
+    { 0,       -23,         256, 1 },
+    { 16,      -16,         256, 1 },
+    { 23,      0,           256, 1 },
+    { 16,      16,          256, 1 },
+    { 0,       23,          256, 1 },
+    { -16,      16,         256, 1 },
+
+    { -23,      0,          256, 1 },
+    { -16,      -16,        256, 1 },
+    { 0,        -16,        256, 1 },
+    { 16,       -16,        256, 1 },
+    { 16,       0,          256, 1 },
+    { 16,       16,         256, 1 },
+    { 0,        16,         256, 1 },
+    { -16,      16,         256, 1 },
+    { -16,       0,         256, 1 },
+};
+
 BaseGameObject* MainMenuTransition::VDestructor(signed int flags)
 {
     return Vdtor_4369F0(flags);
@@ -491,7 +531,7 @@ MainMenuTransition* MainMenuTransition::ctor_436370(__int16 layer, __int16 fadeD
         field_10_current_Value = 255;
     }
 
-    field_242 = 0;
+    field_242_idx = 0;
     field_246_colour_fade_value = 0;
     field_23E_width = 320;
     field_240_k120 = 120;
@@ -555,9 +595,114 @@ void MainMenuTransition::VRender(int** ppOt)
     VRender_436610(ppOt);
 }
 
-void MainMenuTransition::VRender_436610(int** /*ppOt*/)
+void MainMenuTransition::VRender_436610(int** ot)
 {
-    NOT_IMPLEMENTED();
+    // TODO: The fixed point math/var needs cleaning up/refactoring in here
+    int currentValue = field_10_current_Value;
+    int v4 = (currentValue + 1) >> 4;
+    int v5 = v4 * v4 * v4 * v4 >> 8;
+
+    int bValue = v4 * v4 * v4 * v4 >> 8;
+    if (v5 > 255)
+    {
+        bValue = -1; // LOBYTE
+        v5 = 255;
+    }
+
+    int rgValue = v5 * v5 >> 8;
+    if (rgValue > 255)
+    {
+        rgValue = -1; // LOBYTE
+    }
+
+    int op1 = currentValue << 12;
+    int val1 = Math_Cosine_4510A0(field_246_colour_fade_value).fpValue;
+    int val2 = Math_Sine_451110(field_246_colour_fade_value).fpValue;
+    int r0g0 = -64 / ((v5 >> 2) + 1);
+    for (int i = 0; i < 8; i++)
+    {
+        int idx = i + (8 * this->field_242_idx);
+        int v8 = stru_55C038[idx].field_4 << 8;
+        int v9 = stru_55C038[idx].field_2 << 16;
+        int v10 = (signed __int16)stru_55C038[idx].field_0 << 16;
+        int v11 = Math_FixedPoint_Multiply_451040(v9, val1);
+        int v12 = Math_FixedPoint_Multiply_451040(v10, val2) - v11;
+        int v13 = Math_FixedPoint_Multiply_451040(op1, v8);
+        __int16 x0 = this->field_23E_width + 640 * ((signed int)Math_FixedPoint_Multiply_451040(v12, v13) >> 16) / 368;
+        int v14 = Math_FixedPoint_Multiply_451040(v9, val2);
+        int v15 = Math_FixedPoint_Multiply_451040(v10, val1) + v14;
+        int v16 = Math_FixedPoint_Multiply_451040(op1, v8);
+        __int16 y0 = this->field_240_k120 + (Math_FixedPoint_Multiply_451040(v15, v16) >> 16);
+
+        int v17 = 0;
+        if (i < 7)
+        {
+            v17 = i + 1;
+        }
+        else
+        {
+            v17 = 0;
+        }
+
+        int idx2 = (8 * this->field_242_idx);
+        int v36 = (signed __int16)stru_55C038[idx2 + v17].field_0 << 16;
+        int v19 = 0;
+        if (i < 7)
+        {
+            v19 = i + 1;
+        }
+        else
+        {
+            v19 = 0;
+        }
+
+        int v20 = stru_55C038[idx2 + v19].field_2 << 16;
+        int v38 = v20;
+        int v21 = 0;
+        if (i < 7)
+        {
+            v21 = i + 1;
+        }
+        else
+        {
+            v21 = 0;
+        }
+        int y1 = stru_55C038[v21 + idx2].field_4 << 8;
+
+        int v23 = Math_FixedPoint_Multiply_451040(v20, val1);
+        int x1 = Math_FixedPoint_Multiply_451040(v36, val2) - v23;
+        int v25 = Math_FixedPoint_Multiply_451040(op1, y1);
+        // TODO: Use PsxToPCX
+        x1 = this->field_23E_width + 40 * ((signed int)Math_FixedPoint_Multiply_451040(x1, v25) >> 16) / 23; // LOWORD
+        int v26 = Math_FixedPoint_Multiply_451040(v38, val2);
+        int v27 = v26 + Math_FixedPoint_Multiply_451040(v36, val1);
+        int v28 = Math_FixedPoint_Multiply_451040(op1, y1);
+        y1 = this->field_240_k120 + (Math_FixedPoint_Multiply_451040(v27, v28) >> 16); // LOWORD
+        Poly_G3* pPoly = &field_1C_polys[gPsxDisplay_504C78.field_A_buffer_index].field_0_polys[i];
+
+        SetRGB0(pPoly, static_cast<BYTE>(r0g0), static_cast<BYTE>(r0g0), 255);
+        SetRGB1(pPoly, static_cast<BYTE>(rgValue), static_cast<BYTE>(rgValue), static_cast<BYTE>(bValue));
+        SetRGB2(pPoly, static_cast<BYTE>(rgValue), static_cast<BYTE>(rgValue), static_cast<BYTE>(bValue));
+
+        SetXY0(pPoly, field_23E_width, field_240_k120);
+        SetXY1(pPoly, x0, y0);
+        SetXY2(pPoly, static_cast<short>(x1), static_cast<short>(y1));
+
+        OrderingTable_Add_498A80(&ot[field_23C_layer], &pPoly->mBase.header);
+    }
+
+    OrderingTable_Add_498A80(&ot[field_23C_layer], &field_21C_tPage[gPsxDisplay_504C78.field_A_buffer_index].mBase);
+
+    pScreenManager_4FF7C8->InvalidateRect_406CC0(0, 0, gPsxDisplay_504C78.field_0_width, gPsxDisplay_504C78.field_2_height);
+
+    if ((field_10_current_Value == 255 && field_14_fade_direction) || (field_10_current_Value == 0 && !field_14_fade_direction))
+    {
+        field_16_bDone = 1;
+        if (field_18_bKillOnDone)
+        {
+            field_6_flags.Set(BaseGameObject::eDead_Bit3);
+        }
+    }
 }
 
 Menu* Menu::ctor_47A6F0(Path_TLV* /*pTlv*/, int tlvInfo)
