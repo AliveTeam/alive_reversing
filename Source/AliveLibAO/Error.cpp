5fd0fcdc30091df428f442bba3f14ad517bd48b0
Paul <github@paulsapps.com>
2020-11-23 10:44:25 +0000

    make AE_IMPLEMENTED actually work, remove usage of both NOT_IMPLEMENTED and AE_IMPLEMENTED as this will now have unexpected results

--- Source/AliveLibAO/Error.cpp
+++ Source/AliveLibAO/Error.cpp
@@ -7,7 +7,6 @@ namespace AO {
 
 EXPORT void Error_WarningMessageBox_48E470(const char* pWarningMsg, ...)
 {
-    NOT_IMPLEMENTED();
     AE_IMPLEMENTED();
     // HACK: VA_ARGS can't be forwarded, but no big deal as this will be resolved when engines are merged
     Error_WarningMessageBox_4F2D80(pWarningMsg);
@@ -15,7 +14,6 @@ EXPORT void Error_WarningMessageBox_48E470(const char* pWarningMsg, ...)
 
 EXPORT void CC Error_ShowErrorStackToUser_48DF10(bool bDisplayAsOneMessage)
 {
-    NOT_IMPLEMENTED();
     AE_IMPLEMENTED();
     Error_ShowErrorStackToUser_4F2A70(bDisplayAsOneMessage);
 }
--- Source/AliveLibAO/Sys.cpp
+++ Source/AliveLibAO/Sys.cpp
@@ -25,28 +25,24 @@ EXPORT LPSTR CC Sys_GetCommandLine_48E920()
 
 EXPORT TWindowHandleType CC Sys_GetWindowHandle_48E930()
 {
-    NOT_IMPLEMENTED();
     AE_IMPLEMENTED();
     return Sys_GetWindowHandle_4EE180();
 }
 
 EXPORT void CC Sys_Set_Hwnd_48E340(TWindowHandleType hwnd)
 {
-    NOT_IMPLEMENTED();
     AE_IMPLEMENTED();
     Sys_Set_Hwnd_4F2C50(hwnd);
 }
 
 EXPORT int CC Sys_WindowClass_Register_48E9E0(LPCSTR lpClassName, LPCSTR lpWindowName, int X, int Y, int nWidth, int nHeight)
 {
-    NOT_IMPLEMENTED();
     AE_IMPLEMENTED();
     return Sys_WindowClass_Register_4EE22F(lpClassName, lpWindowName, X, Y, nWidth, nHeight);
 }
 
 EXPORT void SYS_EventsPump_44FF90()
 {
-    NOT_IMPLEMENTED();
     AE_IMPLEMENTED();
     Sys_PumpMessages_4EE4F4();
 }
