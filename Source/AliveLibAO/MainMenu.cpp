27a9f93146f80a88a3b7c1b9f7ae110f8692af21
V <varrok@gmail.com>
2020-11-25 01:07:33 +0100

    Menu::ButtonRemap_Render_47F940()

--- Source/AliveLibAO/MainMenu.cpp
+++ Source/AliveLibAO/MainMenu.cpp
@@ -2941,9 +2941,196 @@ void Menu::Goto_ConfigureController_OrSave_SettingIni_Update_47F380()
     }
 }
 
-void Menu::ButtonRemap_Render_47F940(int** /*ppOt*/)
+const Menu_Element chooseAndExitRemapButtons_4D0690[2] =
 {
-    NOT_IMPLEMENTED();
+    { 40, 34, 64 },
+    { 301, 163, 2048 }
+};
+
+const Menu_Button remapButtons_4D0170[8] =
+{
+    { 172, 78, 6176 },
+    { 172, 109, 6176 },
+    { 172, 138, 6176 },
+    { 172, 169, 6176 },
+    { 258, 77, 6176 },
+    { 258, 109, 6176 },
+    { 258, 138, 6176 },
+    { 258, 169, 6176 }
+};
+
+const char* inputActions_4D0070[8] =
+{
+    "Run", "Sneak", "Jump", "Speak 1", "Action", "Throw", "Crouch", "Speak 2"
+};
+
+
+void Menu::ButtonRemap_Render_47F940(int** ppOt)
+{
+    int frameTable = 0;
+    if (field_230_bGoBack == -1)
+    {
+        frameTable = remapButtons_4D0170[field_1E0_selected_index].field_4_frame_table;
+    }
+    else
+    {
+        frameTable = remapButtons_4D0170[field_230_bGoBack].field_4_frame_table;
+    }
+    field_134_anim.Set_Animation_Data_402A40(frameTable, 0);
+
+    int polyOffset = 0;
+    for (int i = 0; i < ALIVE_COUNTOF(remapButtons_4D0170); i++)
+    {
+        RenderElement_47A4E0(
+            remapButtons_4D0170[i].field_0_xpos,
+            remapButtons_4D0170[i].field_2_ypos,
+            dword_4D0030.buttons[sJoystickEnabled_508A60 != 0][i],
+            ppOt,
+            &field_FC_font,
+            &polyOffset
+        );
+    }
+
+    if (field_230_bGoBack == -1)
+    {
+        field_134_anim.VRender_403AE0(
+            remapButtons_4D0170[field_1E0_selected_index].field_0_xpos - 3,
+            remapButtons_4D0170[field_1E0_selected_index].field_2_ypos + 1,
+            ppOt,
+            0,
+            0
+        );
+    }
+    else
+    {
+        field_134_anim.VRender_403AE0(
+            remapButtons_4D0170[field_230_bGoBack].field_0_xpos,
+            remapButtons_4D0170[field_230_bGoBack].field_2_ypos,
+            ppOt,
+            0,
+            0
+        );
+    }
+    if (field_230_bGoBack == 8)
+    {
+        const int maxFontWidth = 336;
+        if (sJoystickEnabled_508A60)
+        {
+            field_1F4_text = "Press button to use";
+        }
+        else
+        {
+            field_1F4_text = "Press key to use";
+        }
+        int fontWidth = field_FC_font.MeasureWidth_41C280(field_1F4_text, FP_FromInteger(1));
+        short calculatedXposBasedOnWidth = 0;
+        if (fontWidth >= maxFontWidth)
+        {
+            calculatedXposBasedOnWidth = 16;
+        }
+        else
+        {
+            calculatedXposBasedOnWidth = static_cast<short>((368 - fontWidth) / 2);
+        }
+        int drawnStringOffset = field_FC_font.DrawString_41C360(
+            ppOt,
+            field_1F4_text,
+            calculatedXposBasedOnWidth,
+            88,
+            0,
+            1,
+            0,
+            41,
+            40,
+            20,
+            0,
+            polyOffset,
+            FP_FromInteger(1),
+            640,
+            0);
+        polyOffset = drawnStringOffset;
+
+        char buffer[40] = {};
+        sprintf(buffer, "for %s", inputActions_4D0070[field_1E0_selected_index]);
+        field_1F4_text = buffer;
+        auto fontWidth2 = field_FC_font.MeasureWidth_41C280(buffer, FP_FromInteger(1));
+        short calculatedXposBasedOnWidth2 = 0;
+        if (fontWidth2 >= maxFontWidth)
+        {
+            calculatedXposBasedOnWidth2 = 16;
+        }
+        else
+        {
+            calculatedXposBasedOnWidth2 = static_cast<short>((368 - fontWidth2) / 2);
+        }
+        polyOffset = field_FC_font.DrawString_41C360(
+            ppOt,
+            field_1F4_text,
+            calculatedXposBasedOnWidth2,
+            120,
+            0,
+            1,
+            0,
+            41,
+            40,
+            20,
+            0,
+            polyOffset,
+            FP_FromInteger(1),
+            640,
+            0
+        );
+        field_1F4_text = "or Esc for none";
+        auto fontWidth3 = field_FC_font.MeasureWidth_41C280("or Esc for none", FP_FromInteger(1));
+        short calculatedXposBasedOnWidth3 = 0;
+        if (fontWidth3 >= maxFontWidth)
+        {
+            calculatedXposBasedOnWidth3 = 16;
+        }
+        else
+        {
+            calculatedXposBasedOnWidth3 = static_cast<short>((368 - fontWidth3) / 2);
+        }
+        polyOffset = field_FC_font.DrawString_41C360(
+            ppOt,
+            field_1F4_text,
+            calculatedXposBasedOnWidth3,
+            152,
+            0,
+            1,
+            0,
+            41,
+            40,
+            20,
+            0,
+            polyOffset,
+            FP_FromInteger(1),
+            640,
+            0
+        );
+    }
+
+    PSX_RECT pRect = {};
+    field_134_anim.Get_Frame_Rect_402B50(&pRect);
+    pScreenManager_4FF7C8->InvalidateRect_406E40(
+        pRect.x,
+        pRect.y,
+        pRect.w,
+        pRect.h,
+        pScreenManager_4FF7C8->field_2E_idx
+    );
+
+    for( int i = 0; i < ALIVE_COUNTOF(chooseAndExitRemapButtons_4D0690); i++)
+    {
+        RenderElement_47A4E0(
+            chooseAndExitRemapButtons_4D0690[i].field_0_xpos,
+            chooseAndExitRemapButtons_4D0690[i].field_4_ypos,
+            chooseAndExitRemapButtons_4D0690[i].field_8_input_command,
+            ppOt,
+            &field_FC_font,
+            &polyOffset
+        );
+    }
 }
 
 void Menu::To_ButtonRemap_Update_47F860()
