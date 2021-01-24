e65c8ffc5909c7384ca3b63809af3c41acde6189
Paul <github@paulsapps.com>
2021-01-24 14:16:16 +0000

    try to do #1054 again by starting from scratch and ending up with almost the exact same results

--- Source/AliveLibAO/Paramite.cpp
+++ Source/AliveLibAO/Paramite.cpp
@@ -1095,59 +1095,65 @@ __int16 Paramite::Brain_Patrol_447A10()
         {
             if (VIsFacingMe(sActiveHero_507678))
             {
-                if ((!field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)) ||
-                    (field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2)))
+                if ((!field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2)) ||
+                    (field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)))
                 {
                     field_FE_next_state = eParamiteStates::State_13_GameSpeakBegin_44D050;
                     return AI_Patrol::eState0_StuckToWall_8;
                 }
+
                 Sound_44DBB0(5u);
                 field_FE_next_state = eParamiteStates::State_5_Turn_44C8E0;
                 return AI_Patrol::eState0_TurningForAbe_6;
             }
-
             // close range - run away or attack if cornered
-            if (VIsObjNearby(kGridSize * FP_FromInteger(4), sActiveHero_507678))
+            else if (VIsObjNearby(kGridSize * FP_FromInteger(4), sActiveHero_507678))
+            {
+                if (!(field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)) ||
+                    (field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2)))
+                {
+                    field_FE_next_state = eParamiteStates::State_5_Turn_44C8E0;
+                    return AI_Patrol::eState0_HittingAbe_7;
+                }
+
+                field_FE_next_state = eParamiteStates::State_2_Walking_44B9E0;
+                return AI_Patrol::eState0_FearingAbe_2;
+            }
+            else
+            {
+                if (!(field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)) ||
+                    (field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2)))
+                {
+                    field_FE_next_state = eParamiteStates::State_5_Turn_44C8E0;
+                    return AI_Patrol::eState0_HittingAbe_7;
+                }
+                field_FE_next_state = eParamiteStates::State_3_Running_44C070;
+                return AI_Patrol::eState0_RunningFromAbe_3;
+            }
+        }
+        // far range - follow abe 
+        else if (!VIsObjNearby(kGridSize * FP_FromInteger(8), sActiveHero_507678))
+        {
+            if (!VIsFacingMe(sActiveHero_507678))
+            {
+                if (!field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2) ||
+                    (field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)))
+                {
+                    field_FE_next_state = eParamiteStates::State_5_Turn_44C8E0;
+                    return AI_Patrol::eState0_TurningForAbe_6;
+                }
+
+                field_FE_next_state = eParamiteStates::State_2_Walking_44B9E0;
+                return AI_Patrol::eState0_FearingAbe_2;
+            }
+            else
             {
                 if ((field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)) ||
                     (!field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2)))
                 {
                     field_FE_next_state = eParamiteStates::State_2_Walking_44B9E0;
-                    field_128_never_read = 0;
-                    return AI_Patrol::eState0_FearingAbe_2;
+                    return AI_Patrol::eState0_ApproachingAbe_4;
                 }
-                field_FE_next_state = eParamiteStates::State_3_Running_44C070;
-                return AI_Patrol::eState0_RunningFromAbe_3;
-            }
-
-            if ((field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(1, 2)) ||
-                !field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && Check_IsOnEndOfLine_4021A0(0, 2))
-            {
-                field_FE_next_state = eParamiteStates::State_5_Turn_44C8E0;
-                return AI_Patrol::eState0_HittingAbe_7;
-            }
-
-            field_FE_next_state = eParamiteStates::State_2_Walking_44B9E0;
-            return AI_Patrol::eState0_FearingAbe_2;
-        }
-
-        // far range - follow abe 
-        if (!VIsObjNearby(kGridSize * FP_FromInteger(8), sActiveHero_507678))
-        {
-            if (!VIsFacingMe(sActiveHero_507678))
-            {
-                if ((field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && !Check_IsOnEndOfLine_4021A0(0, 2)) ||
-                    (!field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && !Check_IsOnEndOfLine_4021A0(1, 2)))
-                {
-                    field_FE_next_state = eParamiteStates::State_5_Turn_44C8E0;
-                    return AI_Patrol::eState0_TurningForAbe_6;
-                }
-            }
-            else if ((field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && !Check_IsOnEndOfLine_4021A0(1, 2)) ||
-                    (!field_10_anim.field_4_flags.Get(AnimFlags::eBit5_FlipX) && !Check_IsOnEndOfLine_4021A0(0, 2)))
-            {
-                field_FE_next_state = eParamiteStates::State_2_Walking_44B9E0;
-                return AI_Patrol::eState0_ApproachingAbe_4;
             }
         }
 
