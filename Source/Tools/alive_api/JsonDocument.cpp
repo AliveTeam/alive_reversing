79da3fedfaf24ab5a74dc84c1372a114e3ee453f
Paul <github@paulsapps.com>
2021-03-20 06:24:51 +0000

    remove temp test code

--- Source/Tools/alive_api/JsonDocument.cpp
+++ Source/Tools/alive_api/JsonDocument.cpp
@@ -314,19 +314,6 @@ jsonxx::Array JsonWriterAO::ReadTlvStream(TypesCollection& globalTypes, BYTE* pt
     {
         while (pPathTLV)
         {
-            if (pPathTLV->field_4_type == AO::TlvTypes::RingCancel_109)
-            {
-                struct Hack_TLV : public AO::Path_TLV
-                {
-                    __int16 whatThis;
-                };
-                auto pHack = reinterpret_cast<Hack_TLV*>(pPathTLV);
-                if (pHack->whatThis != 0)
-                {
-                    abort();
-                }
-            }
-
             auto obj = globalTypes.MakeTlvAO(pPathTLV->field_4_type.mType, pPathTLV);
             if (obj)
             {
