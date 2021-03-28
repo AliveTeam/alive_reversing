7b73c6381f66f7c4a02630d1597e750727e1334e
mouzedrift <mouzedrift@gmail.com>
2021-03-28 19:17:44 +0200

    clean up Mudokon::StableDelay_477570()

--- Source/AliveLibAE/Mudokon.cpp
+++ Source/AliveLibAE/Mudokon.cpp
@@ -6984,13 +6984,7 @@ void Mudokon::M_TurnWheelEnd_59_474D30()
 
 __int16 Mudokon::StableDelay_477570()
 {
-    int maxIdx = sAlertedMudCount_5C3010;
-    if (sAlertedMudCount_5C3010 >= ALIVE_COUNTOF(kDelayTable_55CF7C))
-    {
-        maxIdx = ALIVE_COUNTOF(kDelayTable_55CF7C);
-    }
-
-    if (sDelayIdx_5C3014 >= maxIdx)
+    if (sDelayIdx_5C3014 >= ALIVE_COUNTOF(kDelayTable_55CF7C))
     {
         sDelayIdx_5C3014 = 0;
     }
