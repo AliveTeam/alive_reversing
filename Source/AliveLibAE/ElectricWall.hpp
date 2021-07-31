feb85bef4052ca987de7c594a182dc0775efb0a0
mouzedrift <mouzedrift@gmail.com>
2021-07-31 12:03:21 +0200

    fix build

--- Source/AliveLibAE/ElectricWall.hpp
+++ Source/AliveLibAE/ElectricWall.hpp
@@ -4,7 +4,7 @@
 #include "Path.hpp"
 #include "FunctionFwd.hpp"
 
-enum StartState : s16
+enum ElectricWallStartState : s16
 {
     eOff_0 = 0,
     eOn_1 = 1,
@@ -14,7 +14,7 @@ struct Path_ElectricWall final : public Path_TLV
 {
     Scale_short field_10_scale;
     s16 field_12_id;
-    StartState field_14_start_state;
+    ElectricWallStartState field_14_start_state;
     s16 field_16_pad;
 };
 ALIVE_ASSERT_SIZEOF_ALWAYS(Path_ElectricWall, 0x18);
@@ -36,7 +36,7 @@ private:
 private:
     s32 field_F4_tlvInfo;
     s16 field_F8_switch_id;
-    StartState field_FA_start_state;
+    ElectricWallStartState field_FA_start_state;
     s32 field_FC_sound_timer;
 };
 ALIVE_ASSERT_SIZEOF(ElectricWall, 0x100);
