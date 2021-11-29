9b778e3a5be0f9628e518cf5731d2c0638b9fd2d
Paul <github@paulsapps.com>
2021-11-29 01:22:00 +0000

    more slashes

--- Source/AliveLibAE/stdafx.h
+++ Source/AliveLibAE/stdafx.h
@@ -5,5 +5,5 @@
 
 #pragma once
 
-#include "..\AliveLibCommon\pch_shared.h"
-#include "..\AliveLibCommon\relive_config.h"
+#include "../AliveLibCommon/pch_shared.h"
+#include "../AliveLibCommon/relive_config.h"
