a27ed75fd19b17942b1f3d510b9e0657ed648124
Paul <github@paulsapps.com>
2021-12-03 23:26:33 +0000

    Export all AO/AE FG1 and cam images correctly

--- Source/Tools/relive_api/CamConverter.cpp
+++ Source/Tools/relive_api/CamConverter.cpp
@@ -10,6 +10,57 @@
 
 namespace ReliveAPI {
 
+    
+static u32 RGB565ToRGB888(u16 pixel)
+{
+    const u8 r5 = ((pixel >> 11) & 0x1F);
+    const u8 g6 = ((pixel >> 5) & 0x3F);
+    const u8 b5 = (pixel & 0x1F);
+
+    const u32 r8 = ((r5 * 527) + 23) >> 6;
+    const u32 g8 = ((g6 * 259) + 33) >> 6;
+    const u32 b8 = ((b5 * 527) + 23) >> 6;
+
+    const u32 rgb888 = (b8 << 16) | (g8 << 8) | r8;
+    return rgb888;
+}
+
+static void SaveCamPng(const u16* camBuffer, const char_type* pFileName)
+{
+    u32 dst[240][640] = {};
+    for (u32 y = 0; y < 240; y++)
+    {
+        for (u32 x = 0; x < 640; x++)
+        {
+            dst[y][x] = RGB565ToRGB888(*camBuffer);
+            camBuffer++;
+        }
+    }
+
+    // we're going to encode with a state rather than a convenient function, because enforcing a color type requires setting options
+    lodepng::State state;
+    // input color type
+    state.info_raw.colortype = LCT_RGBA;
+    state.info_raw.bitdepth = 8;
+
+    // output color type
+    state.info_png.color.colortype = LCT_RGB;
+    state.info_png.color.bitdepth = 8;
+    state.encoder.auto_convert = 0; // without this, it would ignore the output color type specified above and choose an optimal one instead
+
+    // encode and save
+    std::vector<u8> buffer;
+    const auto error = lodepng::encode(buffer, reinterpret_cast<const u8*>(&dst[0][0]), 640, 240, state);
+    if (error)
+    {
+        std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    }
+    else
+    {
+        lodepng::save_file(buffer, pFileName);
+    }
+}
+
 struct FG1Buffers
 {
     void SetPixel(u32 layer, u32 x, u32 y, u16 pixel)
@@ -34,20 +85,6 @@ public:
         delete mFg1Buffers;
     }
 
-    bool LayerUsed(u32 idx) const
-    {
-        if (idx < 4)
-        {
-            return mUsedLayers[idx];
-        }
-        return false;
-    }
-
-    const FG1Buffers& Fg1Data() const
-    {
-        return *mFg1Buffers;
-    }
-
     u16 ConvertPixel(u16 pixel)
     {
         return ((pixel >> 15) << 5)
@@ -56,30 +93,39 @@ public:
                       | (((pixel >> 10) & 31) << 0);
     }
 
-    void BltRect(u32 xpos, u32 ypos, u32 width, u32 height, u32 layer, const u16* pSrc)
+    void BltRect(u32 xpos, u32 ypos, u32 width, u32 height, u32 layer, const u16* pSrcPixels, const u32* pBitMask)
     {
         mUsedLayers[layer] = true;
 
-        for (u32 x = xpos; x < width; x++)
+        for (u32 y = ypos; y < height; y++)
         {
-            for (u32 y = ypos; y < height; y++)
+            for (u32 x = xpos; x < width; x++)
             {
                 if (x < 640 && y < 240)
                 {
-                    u16 pixelVal = 0xFF;
-                    if (pSrc)
+                    u16 pixelVal = 0xFFFF;
+                    if (pSrcPixels)
                     {
-                        if (mFormat == FG1Format::AO)
+                        // Read RGB565 pixel value for AO
+                        pixelVal = ConvertPixel(*pSrcPixels);
+
+                        // If its not a "transparent" pixel set to white
+                        if (pixelVal != 0)
                         {
-                            // Read RGB565 pixel value
-                            pixelVal = ConvertPixel(*pSrc);
-                            pSrc++;
+                            pixelVal = 0xFFFF;
                         }
-                        else
+                        pSrcPixels++;
+                    }
+                    else if (pBitMask)
+                    {
+                        // Bitfield in AE
+                        const u32 bits = pBitMask[y - ypos];
+                        if (!((bits >> (x - xpos)) & 1))
                         {
-                            // TODO: Bitfield in AE
+                            pixelVal = 0;
                         }
                     }
+
                     mFg1Buffers->SetPixel(layer, x, y, pixelVal);
                 }
             }
@@ -88,13 +134,23 @@ public:
 
     void OnPartialChunk(const Fg1Chunk& rChunk) override
     {
-        const u16* pPixels = reinterpret_cast<const u16*>((&rChunk) + 1);
+        const u16* pPixels = nullptr;
+        const u32* pBitMap = nullptr;
+        if (mFormat == FG1Format::AO)
+        {
+            pPixels = reinterpret_cast<const u16*>((&rChunk) + 1);
+        }
+        else
+        {
+            pBitMap = reinterpret_cast<const u32*>((&rChunk) + 1);
+        }
         BltRect(rChunk.field_4_xpos_or_compressed_size,
                 rChunk.field_6_ypos,
                 rChunk.field_8_width + rChunk.field_4_xpos_or_compressed_size,
                 rChunk.field_A_height + rChunk.field_6_ypos,
                 rChunk.field_2_layer_or_decompressed_size,
-                pPixels);
+                pPixels,
+                pBitMap);
     }
 
     void OnFullChunk(const Fg1Chunk& rChunk) override
@@ -104,6 +160,7 @@ public:
                 rChunk.field_8_width + rChunk.field_4_xpos_or_compressed_size,
                 rChunk.field_A_height + rChunk.field_6_ypos,
                 rChunk.field_2_layer_or_decompressed_size,
+                nullptr,
                 nullptr);
     }
 
@@ -120,6 +177,18 @@ public:
         delete ptr;
     }
 
+    void SaveLayers(const std::string& baseName)
+    {
+        for (u32 i = 0; i < 4; i++)
+        {
+            if (mUsedLayers[i])
+            {
+                const u16* pPixels = &mFg1Buffers->mFg1[i][0][0];
+                SaveCamPng(pPixels, (baseName + "_" + std::to_string(i + 1) + "_FG1.png").c_str());
+            }
+        }
+    }
+
 private:
     // 2 layers in AO, 4 layers in AE
     bool mUsedLayers[4] = {};
@@ -132,56 +201,6 @@ static void AppendCamSegment(s32 x, s32 y, s32 width, s32 height, u16* pDst, con
     for (s32 y1 = y; y1 < height; y1++)
     {
         memcpy(&pDst[x + (y1 * 640)], &pSrc[(y1 * width)], width * sizeof(s16));
-    }
-}
-
-static u32 RGB565ToRGB888(u16 pixel)
-{
-    const u8 r5 = ((pixel >> 11) & 0x1F);
-    const u8 g6 = ((pixel >> 5) & 0x3F);
-    const u8 b5 = (pixel & 0x1F);
-
-    const u32 r8 = ((r5 * 527) + 23) >> 6;
-    const u32 g8 = ((g6 * 259) + 33) >> 6;
-    const u32 b8 = ((b5 * 527) + 23) >> 6;
-
-    const u32 rgb888 = (b8 << 16) | (g8 << 8) | r8;
-    return rgb888;
-}
-
-/*static*/ void SaveCamPng(const u16* camBuffer, const char_type* pFileName)
-{
-    u32 dst[240][640] = {};
-    for (u32 y = 0; y < 240; y++)
-    {
-        for (u32 x = 0; x < 640; x++)
-        {
-            dst[y][x] = RGB565ToRGB888(*camBuffer);
-            camBuffer++;
-        }
-    }
-
-    // we're going to encode with a state rather than a convenient function, because enforcing a color type requires setting options
-    lodepng::State state;
-    // input color type
-    state.info_raw.colortype = LCT_RGBA;
-    state.info_raw.bitdepth = 8;
-
-    // output color type
-    state.info_png.color.colortype = LCT_RGB;
-    state.info_png.color.bitdepth = 8;
-    state.encoder.auto_convert = 0; // without this, it would ignore the output color type specified above and choose an optimal one instead
-
-    //encode and save
-    std::vector<u8> buffer;
-    const auto error = lodepng::encode(buffer, reinterpret_cast<const u8*>(&dst[0][0]), 640, 240, state);
-    if (error)
-    {
-        std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-    }
-    else
-    {
-        lodepng::save_file(buffer, pFileName);
     }
 }
 
@@ -210,19 +229,7 @@ CamConverterAO::CamConverterAO(const std::string& fileName, const ChunkedLvlFile
     {
         ApiFG1Reader reader(BaseFG1Reader::FG1Format::AO);
         reader.Iterate(reinterpret_cast<const FG1ResourceBlockHeader*>(fg1Res->Data().data()));
-        if (fileName == "R1P19C01.png")
-        {
-            const FG1Buffers& data = reader.Fg1Data();
-            for (u32 i = 0; i < 4; i++)
-            {
-                if (reader.LayerUsed(i))
-                {
-                    const u16* pPixels = &data.mFg1[i][0][0];
-                    SaveCamPng(pPixels, (fileName + "FG1.png").c_str());
-                }
-            }
-        }
-        // TODO: Save layers
+        reader.SaveLayers(fileName.substr(0, fileName.length() - 4));
     }
 }
 
@@ -257,7 +264,7 @@ CamConverterAE::CamConverterAE(const std::string& fileName, const ChunkedLvlFile
     {
         ApiFG1Reader reader(BaseFG1Reader::FG1Format::AE);
         reader.Iterate(reinterpret_cast<const FG1ResourceBlockHeader*>(fg1Res->Data().data()));
-        // TODO: Save layers
+        reader.SaveLayers(fileName.substr(0, fileName.length()- 4));
     }
 }
 } // namespace ReliveAPI
