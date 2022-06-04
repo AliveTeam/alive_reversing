b50ad20d18e77ab8675c0f26815bd96ec92e20cd
Paul <github@paulsapps.com>
2022-06-04 20:51:39 +0100

    fix missing case

--- Source/AliveLibAE/BirdPortal.cpp
+++ Source/AliveLibAE/BirdPortal.cpp
@@ -960,7 +960,7 @@ s16 BirdPortal::IsScaredAway()
         switch (pObj->Type())
         {
             case ReliveTypes::eAbe:
-            //case ReliveTypes::eMudokon2:
+            case ReliveTypes::eCtorMudokon:
             case ReliveTypes::eParamite:
             case ReliveTypes::eScrab:
             case ReliveTypes::eSlig:
