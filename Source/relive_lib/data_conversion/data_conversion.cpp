eb2baee051cca801d70efedafd266c0aa59c5fa8
Paul <github@paulsapps.com>
2022-07-08 02:05:33 +0100

    extract lvls to their own dirs

--- Source/relive_lib/data_conversion/data_conversion.cpp
+++ Source/relive_lib/data_conversion/data_conversion.cpp
@@ -6,12 +6,57 @@
 #include "../MapWrapper.hpp"
 #include <vector>
 
+constexpr u32 kDataVersion = 1;
 
+static const char* ToString(AO::LevelIds lvlId)
+{
+    switch (lvlId)
+    {
+        case AO::LevelIds::eMenu_0:
+            return "menu";
+        case AO::LevelIds::eRuptureFarms_1:
+            return "rupture_farms";
+        case AO::LevelIds::eLines_2:
+            return "monsaic_lines";
+        case AO::LevelIds::eForest_3:
+            return "menu";
+        case AO::LevelIds::eForestTemple_4:
+            return "paramonia_temple";
+        case AO::LevelIds::eStockYards_5:
+            return "stock_yards";
+        case AO::LevelIds::eStockYardsReturn_6:
+            return "stock_yards_return";
+        case AO::LevelIds::eDesert_8:
+            return "scrabania";
+        case AO::LevelIds::eDesertTemple_9:
+            return "scrabania_temple";
+        case AO::LevelIds::eCredits_10:
+            return "credits";
+        case AO::LevelIds::eBoardRoom_12:
+            return "board_room";
+        case AO::LevelIds::eRuptureFarmsReturn_13:
+            return "rupture_farms_return";
+        case AO::LevelIds::eForestChase_14:
+            return "paramonia_temple_escape";
+        case AO::LevelIds::eDesertEscape_15:
+            return "scrabania_temple_escape";
+        default:
+            return "unknown";
+    }
+}
 
 void DataConversion::ConvertData()
 {
-    //FileSystem fs;
+    // TODO: Check existing data version, if any
+
+    FileSystem fs;
     //CameraConverter cc;
+
+    FileSystem::Path dataDir;
+    dataDir.Append("relive_data");
+    fs.CreateDirectory(dataDir);
+    dataDir.Append("ao");
+    fs.CreateDirectory(dataDir);
 
     std::vector<u8> fileBuffer;
     for (s32 lvlIdx = 0; lvlIdx < AO::Path_Get_Paths_Count(); lvlIdx++)
@@ -29,11 +74,18 @@ void DataConversion::ConvertData()
                 auto pFileRec = archive.FileAt(i);
                 if (pFileRec->field_0_file_name)
                 {
-                    std::string name(pFileRec->field_0_file_name, strnlen(pFileRec->field_0_file_name, 12));
+                    std::string fileName(pFileRec->field_0_file_name, strnlen(pFileRec->field_0_file_name, 12));
 
                     fileBuffer.resize(pFileRec->field_10_num_sectors * 2048);
                     archive.Read_File(pFileRec, fileBuffer.data());
                     fileBuffer.resize(pFileRec->field_14_file_size);
+
+                    FileSystem::Path filePath;
+                    filePath.Append("relive_data").Append("ao").Append(ToString(static_cast<AO::LevelIds>(lvlIdx)));
+                    fs.CreateDirectory(filePath);
+                    filePath.Append(fileName);
+
+                    fs.Save(filePath, fileBuffer);
 
                     //LOG_INFO(name);
                 }
--- Source/relive_lib/data_conversion/file_system.hpp
+++ Source/relive_lib/data_conversion/file_system.hpp
@@ -1,7 +1,68 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include "../../AliveLibCommon/Sys_common.hpp"
+
+constexpr inline char kDirSeperator = '\\';
+
+// TODO: Lots of stuff missing, needs to do utf8 -> utf16 on windows
 class FileSystem final
 {
 public:
+    class Path final
+    {
+    public:
+        Path& Append(const std::string& directory)
+        {
+            if (!directory.empty() && directory[directory.size() - 1] == kDirSeperator)
+            {
+                ALIVE_FATAL("Directory ends with slash");
+            }
+            if (!mPath.empty())
+            {
+                mPath += kDirSeperator;
+            }
+            mPath += directory;
+            return *this;
+        }
+
+        Path Parent() const
+        {
+            const auto pos = mPath.find_last_of(kDirSeperator);
+            if (pos != std::string::npos)
+            {
+                Path parent;
+                parent.mPath = mPath.substr(0, pos);
+                return parent;
+            }
+            return {};
+        }
+
+        const std::string& GetPath() const
+        {
+            return mPath;
+        }
+
+    private:
+        std::string mPath;
+    };
+
+    void Save(const Path& path, const std::vector<u8>& data)
+    {
+        CreateDirectory(path.Parent());
+        FILE* pFile = ::fopen(path.GetPath().c_str(), "wb");
+        if (pFile)
+        {
+            ::fwrite(data.data(), 1, data.size(), pFile);
+            ::fclose(pFile);
+        }
+    }
+
+    void CreateDirectory(const Path& path)
+    {
+        // NOTE: Creates the last part of the dir only
+        ::CreateDirectoryA(path.GetPath().c_str(), nullptr);
+    }
 
 };
