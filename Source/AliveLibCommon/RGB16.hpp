713d089c6fb6f946a31e202603ccb6519f5e4b98
mouzedrift <mouzedrift@ubuntu.ubuntu-domain>
2022-10-09 03:18:33 +0200

    add required files for build

---
+++ Source/AliveLibCommon/RGB16.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+struct RGB16 final
+{
+    s16 r = 0;
+    s16 g = 0;
+    s16 b = 0;
+
+    void SetRGB(s16 rValue, s16 gValue, s16 bValue)
+    {
+        r = rValue;
+        g = gValue;
+        b = bValue;
+    }
+};
