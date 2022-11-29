3008e1fcf381d275df5927d46c8371a9e50bbbac
Rory Fewell <roryf@oddmatics.uk>
2022-11-29 09:46:47 +0000

    clamp to edge on framebuffer textures - fixes wrapping on filter

--- Source/AliveLibAE/Renderer/OpenGL3/GLFramebuffer.cpp
+++ Source/AliveLibAE/Renderer/OpenGL3/GLFramebuffer.cpp
@@ -22,6 +22,9 @@ GLFramebuffer::GLFramebuffer(u32 width, u32 height)
 
     GL_VERIFY(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0));
 
+    GL_VERIFY(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+    GL_VERIFY(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+
     GL_VERIFY(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
     GL_VERIFY(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
 
