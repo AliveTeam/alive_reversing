4e32297f875d298dac625bc0c24d46da417b8eea
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-02-17 22:16:00 +0100

    surely this will werk

--- Scripts/appveyor_discord_webhook.py
+++ Scripts/appveyor_discord_webhook.py
@@ -8,15 +8,16 @@ BUILD_STATUS = sys.argv[1]
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 if WEBHOOK_URL is None:
+    print("WEBHOOK_URL env var not set")
     sys.exit(0)
 
-BUILD_ID = os.environ["APPVEYOR_BUILD_ID"]
-COMMIT_AUTHOR = os.environ["APPVEYOR_REPO_COMMIT_AUTHOR"]
-COMMIT_MESSAGE = os.environ["APPVEYOR_REPO_COMMIT_MESSAGE"]
-BUILD_VERSION = os.environ["APPVEYOR_BUILD_VERSION"]
-JOB_NAME = os.environ["APPVEYOR_JOB_NAME"]
-REPO_NAME = os.environ["APPVEYOR_REPO_NAME"]
-COMMIT_ID = os.environ["APPVEYOR_REPO_COMMIT"]
+BUILD_ID = os.environ.get("APPVEYOR_BUILD_ID")
+COMMIT_AUTHOR = os.environ.get("APPVEYOR_REPO_COMMIT_AUTHOR")
+COMMIT_MESSAGE = os.environ.get("APPVEYOR_REPO_COMMIT_MESSAGE")
+BUILD_VERSION = os.environ.get("APPVEYOR_BUILD_VERSION")
+JOB_NAME = os.environ.get("APPVEYOR_JOB_NAME")
+REPO_NAME = os.environ.get("APPVEYOR_REPO_NAME")
+COMMIT_ID = os.environ.get("APPVEYOR_REPO_COMMIT")
 COMMIT_URL = f"https://github.com/{REPO_NAME}/commit/{COMMIT_ID}"
 
 ANNOYING_SLURG_EMOTE = "https://cdn.discordapp.com/emojis/803379764270333953.webp"
@@ -26,6 +27,7 @@ COLOR_RED = 12058624
 COLOR_GREEN = 47119
 
 if BUILD_STATUS == "":
+    print("unknown build status")
     sys.exit(0)
 
 footer_text = ""
@@ -77,5 +79,5 @@ webhook_message = {
 
 req = urllib.request.Request(WEBHOOK_URL, json.dumps(webhook_message).encode())
 req.add_header("Content-Type", "application/json")
-req.add_header("X-Author", "mouzedrift")
+req.add_header("User-Agent", "Mozilla/5.0")
 response = urllib.request.urlopen(req)
